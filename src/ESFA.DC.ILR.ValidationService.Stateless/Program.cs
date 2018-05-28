d9824a247d8129999626b21fae9d4052c0806f8b
Sai Siddula <Sai.Siddula@gmail.com>
2018-05-28 20:40:37 +0100

    minor changes for fixing with local.5node values

--- src/ESFA.DC.ILR.ValidationService.Stateless/Program.cs
+++ src/ESFA.DC.ILR.ValidationService.Stateless/Program.cs
@@ -139,7 +139,7 @@ namespace ESFA.DC.ILR.ValidationService.Stateless
 
             containerBuilder.RegisterType<MessageHandler>().As<IMessageHandler>();
 
-            // register the  callback handle when a new message is received from SeriveBus
+            // register the  callback handle when a new message is received from ServiceBus
             containerBuilder.Register<Func<JobContextMessage, CancellationToken, Task<bool>>>(c =>
                 c.Resolve<IMessageHandler>().Handle);
 
