3ac8f998b634c3210deb14d8b313f62ce8c24058
Sai Siddula <Sai.Siddula@gmail.com>
2018-05-30 09:43:24 +0100

    Added Azure cosmos options

---
+++ src/ESFA.DC.ILR.ValidationService.Stateless.Models/AzureCosmosKeyValuePersistenceConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.IO.AzureCosmos.Config.Interfaces;
+
+namespace ESFA.DC.ILR.ValidationService.Stateless.Models
+{
+    public class AzureCosmosKeyValuePersistenceConfig : IAzureCosmosKeyValuePersistenceServiceConfig
+    {
+        public AzureCosmosKeyValuePersistenceConfig(string endpointUrl, string authKeyOrResourceToken)
+        {
+            EndpointUrl = endpointUrl;
+            AuthKeyOrResourceToken = authKeyOrResourceToken;
+        }
+
+        public string EndpointUrl { get; }
+
+        public string AuthKeyOrResourceToken { get; }
+    }
+}
