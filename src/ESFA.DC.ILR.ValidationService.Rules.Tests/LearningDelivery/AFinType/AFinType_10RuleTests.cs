72476d15a8245736ffc7afa44756d36b2b8b3835
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-07-03 09:46:02 +0100

    44487 - AFinType_10Rule implemented

---
+++ src/ESFA.DC.ILR.ValidationService.Rules.Tests/LearningDelivery/AFinType/AFinType_10RuleTests.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ESFA.DC.ILR.Tests.Model;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.AFinType;
+using ESFA.DC.ILR.ValidationService.Rules.Query.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Tests.Abstract;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Tests.LearningDelivery.AFinType
+{
+    public class AFinType_10RuleTests : AbstractRuleTests<AFinType_10Rule>
+    {
+        [Fact]
+        public void RuleName()
+        {
+            NewRule().RuleName.Should().Be("AFinType_10");
+        }
+
+        [Fact]
+        public void FundModelConditionMet_True()
+        {
+            NewRule().FundModelConditionMet(35).Should().BeTrue();
+        }
+
+        [Fact]
+        public void FundModelConditionMet_False()
+        {
+            NewRule().FundModelConditionMet(99).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ApprenticeshipStandardConditionMet_True()
+        {
+            NewRule().ApprenticeshipStandardConditionMet(25, 1).Should().BeTrue();
+        }
+
+        [Fact]
+        public void ApprenticeshipStandardConditionMet_False()
+        {
+            NewRule().ApprenticeshipStandardConditionMet(20, 1).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ApprenticeshipStandardConditionMet_False_NullProgType()
+        {
+            NewRule().ApprenticeshipStandardConditionMet(null, 1).Should().BeFalse();
+        }
+
+        [Fact]
+        public void AppFinRecordConditionMet_True()
+        {
+            var aFinCodes = new List<int> { 2, 4 };
+
+            var aFinRecords = new List<TestAppFinRecord>
+            {
+                new TestAppFinRecord
+                {
+                    AFinType = "TNP",
+                    AFinCode = 1
+                }
+            };
+
+            var learningDeliveryAppFinRecordQueryServiceMock = new Mock<ILearningDeliveryAppFinRecordQueryService>();
+
+            learningDeliveryAppFinRecordQueryServiceMock.Setup(afm => afm.HasAnyLearningDeliveryAFinCodesForType(aFinRecords, "TNP", aFinCodes)).Returns(false);
+
+            NewRule(learningDeliveryAppFinRecordQueryServiceMock.Object).AppFinRecordConditionMet(aFinRecords).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("TNP", 2)]
+        [InlineData("PMR", 2)]
+        [InlineData("PMR", 3)]
+        public void AppFinRecordConditionMet_False(string aFinType, int aFinCode)
+        {
+            var aFinCodes = new List<int> { 2, 4 };
+
+            var aFinRecords = new List<TestAppFinRecord>
+            {
+                new TestAppFinRecord
+                {
+                    AFinType = aFinType,
+                    AFinCode = aFinCode
+                }
+            };
+
+            var learningDeliveryAppFinRecordQueryServiceMock = new Mock<ILearningDeliveryAppFinRecordQueryService>();
+
+            learningDeliveryAppFinRecordQueryServiceMock.Setup(afm => afm.HasAnyLearningDeliveryAFinCodesForType(aFinRecords, "TNP", aFinCodes)).Returns(true);
+
+            NewRule(learningDeliveryAppFinRecordQueryServiceMock.Object).AppFinRecordConditionMet(aFinRecords).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(35, 25, 1, "TNP", 1)]
+        [InlineData(35, 25, 1, "TNP", 3)]
+        [InlineData(35, 25, 1, "PMR", 2)]
+        public void ConditionMet_True(int fundModel, int? progType, int aimType, string aFinType, int aFinCode)
+        {
+            var aFinCodes = new List<int> { 2, 4 };
+
+            var aFinRecords = new List<TestAppFinRecord>
+            {
+                new TestAppFinRecord
+                {
+                    AFinType = aFinType,
+                    AFinCode = aFinCode
+                }
+            };
+
+            var learningDeliveryAppFinRecordQueryServiceMock = new Mock<ILearningDeliveryAppFinRecordQueryService>();
+
+            learningDeliveryAppFinRecordQueryServiceMock.Setup(afm => afm.HasAnyLearningDeliveryAFinCodesForType(aFinRecords, "TNP", aFinCodes)).Returns(false);
+
+            NewRule(learningDeliveryAppFinRecordQueryServiceMock.Object).ConditionMet(fundModel, progType, aimType, aFinRecords).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(99, 25, 1, "TNP", 2)]
+        [InlineData(35, 20, 1, "TNP", 2)]
+        [InlineData(35, 25, 2, "TNP", 2)]
+        [InlineData(35, 25, 1, "TNP", 2)]
+        public void ConditionMet_False(int fundModel, int? progType, int aimType, string aFinType, int aFinCode)
+        {
+            var aFinCodes = new List<int> { 2, 4 };
+
+            var aFinRecords = new List<TestAppFinRecord>
+            {
+                new TestAppFinRecord
+                {
+                    AFinType = aFinType,
+                    AFinCode = aFinCode
+                }
+            };
+
+            var learningDeliveryAppFinRecordQueryServiceMock = new Mock<ILearningDeliveryAppFinRecordQueryService>();
+
+            learningDeliveryAppFinRecordQueryServiceMock.Setup(afm => afm.HasAnyLearningDeliveryAFinCodesForType(aFinRecords, "TNP", aFinCodes)).Returns(true);
+
+            NewRule(learningDeliveryAppFinRecordQueryServiceMock.Object).ConditionMet(fundModel, progType, aimType, aFinRecords).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Validate_Error()
+        {
+            var aFinCodes = new List<int> { 2, 4 };
+
+            var learner = new TestLearner()
+            {
+                LearningDeliveries = new List<TestLearningDelivery>()
+                {
+                    new TestLearningDelivery()
+                    {
+                        FundModel = 35,
+                        ProgTypeNullable = 25,
+                        AimType = 1,
+                        AppFinRecords = new List<TestAppFinRecord>()
+                        {
+                            new TestAppFinRecord()
+                            {
+                                AFinType = "TNP",
+                                AFinCode = 1
+                            }
+                        }
+                    }
+                }
+            };
+
+            var aFinRecords = learner.LearningDeliveries.SelectMany(ld => ld.AppFinRecords);
+
+            var learningDeliveryAppFinRecordQueryServiceMock = new Mock<ILearningDeliveryAppFinRecordQueryService>();
+
+            learningDeliveryAppFinRecordQueryServiceMock.Setup(afm => afm.HasAnyLearningDeliveryAFinCodesForType(aFinRecords, "TNP", aFinCodes)).Returns(false);
+
+            using (var validationErrorHandlerMock = BuildValidationErrorHandlerMockForError())
+            {
+                NewRule(learningDeliveryAppFinRecordQueryServiceMock.Object, validationErrorHandlerMock.Object).Validate(learner);
+            }
+        }
+
+        [Fact]
+        public void Validate_NoError()
+        {
+            var aFinCodes = new List<int> { 2, 4 };
+
+            var learner = new TestLearner()
+            {
+                LearningDeliveries = new List<TestLearningDelivery>()
+                {
+                    new TestLearningDelivery()
+                    {
+                        FundModel = 35,
+                        ProgTypeNullable = 25,
+                        AimType = 1,
+                        AppFinRecords = new List<TestAppFinRecord>()
+                        {
+                            new TestAppFinRecord()
+                            {
+                                AFinType = "TNP",
+                                AFinCode = 2
+                            }
+                        }
+                    }
+                }
+            };
+
+            var aFinRecords = learner.LearningDeliveries.SelectMany(ld => ld.AppFinRecords);
+
+            var learningDeliveryAppFinRecordQueryServiceMock = new Mock<ILearningDeliveryAppFinRecordQueryService>();
+
+            learningDeliveryAppFinRecordQueryServiceMock.Setup(afm => afm.HasAnyLearningDeliveryAFinCodesForType(aFinRecords, "TNP", aFinCodes)).Returns(true);
+
+            using (var validationErrorHandlerMock = BuildValidationErrorHandlerMockForNoError())
+            {
+                NewRule(learningDeliveryAppFinRecordQueryServiceMock.Object, validationErrorHandlerMock.Object).Validate(learner);
+            }
+        }
+
+        private AFinType_10Rule NewRule(ILearningDeliveryAppFinRecordQueryService learningDeliveryAppFinRecordQueryService = null, IValidationErrorHandler validationErrorHandler = null)
+        {
+            return new AFinType_10Rule(learningDeliveryAppFinRecordQueryService, validationErrorHandler);
+        }
+    }
+}
--- src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/AFinType/AFinType_10Rule.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/AFinType/AFinType_10Rule.cs
@@ -11,6 +11,8 @@ namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.AFinType
 {
     public class AFinType_10Rule : AbstractRule, IRule<ILearner>
     {
+        private readonly IEnumerable<int> _aFinCodes = new HashSet<int> { 2, 4 };
+
         private readonly ILearningDeliveryAppFinRecordQueryService _learningDeliveryAppFinRecordQueryService;
 
         public AFinType_10Rule(ILearningDeliveryAppFinRecordQueryService learningDeliveryAppFinRecordQueryService, IValidationErrorHandler validationErrorHandler)
@@ -21,6 +23,47 @@ namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.AFinType
 
         public void Validate(ILearner objectToValidate)
         {
+            foreach (var learningDelivery in objectToValidate.LearningDeliveries)
+            {
+                if (learningDelivery.AppFinRecords != null)
+                {
+                    if (ConditionMet(
+                        learningDelivery.FundModel,
+                        learningDelivery.ProgTypeNullable,
+                        learningDelivery.AimType,
+                        learningDelivery.AppFinRecords))
+                    {
+                        HandleValidationError(objectToValidate.LearnRefNumber, learningDelivery.AimSeqNumber);
+                    }
+                }
+            }
+        }
+
+        public bool ConditionMet(int fundModel, int? progType, int aimType, IEnumerable<IAppFinRecord> appFinRecords)
+        {
+            return FundModelConditionMet(fundModel)
+                && ApprenticeshipStandardConditionMet(progType, aimType)
+                && AppFinRecordConditionMet(appFinRecords);
+        }
+
+        public bool FundModelConditionMet(int fundModel)
+        {
+            return fundModel != 99;
+        }
+
+        public bool ApprenticeshipStandardConditionMet(int? progType, int aimType)
+        {
+            if (progType != null)
+            {
+                return progType == 25 && aimType == 1;
+            }
+
+            return false;
+        }
+
+        public bool AppFinRecordConditionMet(IEnumerable<IAppFinRecord> appFinRecords)
+        {
+            return !_learningDeliveryAppFinRecordQueryService.HasAnyLearningDeliveryAFinCodesForType(appFinRecords, "TNP", _aFinCodes);
         }
     }
 }
