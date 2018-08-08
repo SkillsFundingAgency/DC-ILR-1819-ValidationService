cb501e31a05fd9d43a99f768ce5bc2354fa903e6
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-08-08 17:08:00 +0100

    56741 - RulesetModule Tests update following rebase

--- src/ESFA.DC.ILR.ValidationService.RuleSet.Modules.Tests/RuleSetModuleTests.cs
+++ src/ESFA.DC.ILR.ValidationService.RuleSet.Modules.Tests/RuleSetModuleTests.cs
@@ -216,7 +216,7 @@ namespace ESFA.DC.ILR.ValidationService.RuleSet.Modules.Tests
                 rules.Should().ContainSingle(r => r.GetType() == ruleType);
             }
 
-            rules.Should().HaveCount(133);
+            rules.Should().HaveCount(134);
         }
 
         private void RegisterDependencies(ContainerBuilder builder)
