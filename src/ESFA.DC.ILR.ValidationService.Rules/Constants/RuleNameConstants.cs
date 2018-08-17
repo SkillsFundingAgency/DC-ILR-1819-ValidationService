26f1ba6c2c8370a215114e9c4c96f995c00541bd
saqib2013 <5689652+saqib2013@users.noreply.github.com>
2018-08-17 17:21:02 +0100

    44242 - LearnDelFAMType_44 rule validation implemented and unit tests are added.

--- src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
@@ -122,6 +122,7 @@
         public const string LearnDelFAMType_02 = "LearnDelFAMType_02";
         public const string LearnDelFAMType_03 = "LearnDelFAMType_03";
         public const string LearnDelFAMType_39 = "LearnDelFAMType_39";
+        public const string LearnDelFAMType_44 = "LearnDelFAMType_44";
 
         public const string LearnFAMType_16 = "LearnFAMType_16";
 
