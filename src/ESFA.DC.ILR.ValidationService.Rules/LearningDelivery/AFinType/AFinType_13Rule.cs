34999414677c8521917912b5b00d7d8989545db9
thales-man <msdn@thestripedlawn.co>
2018-08-20 12:21:07 +0100

    afintype 13

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/AFinType/AFinType_13Rule.cs
@@ -0,0 +1,118 @@
+﻿using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Utility;
+using System;
+using System.Linq;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.AFinType
+{
+    /// <summary>
+    /// from version 0.7.1 validation spread sheet
+    /// these rules are singleton's; they can't hold state...
+    /// </summary>
+    /// <seealso cref="Interface.IRule{ILearner}" />
+    public class AFinType_13Rule :
+        IRule<ILearner>
+    {
+        /// <summary>
+        /// Gets the name of the message property.
+        /// </summary>
+        public const string MessagePropertyName = "AFINDATE";
+
+        /// <summary>
+        /// Gets the name of the rule.
+        /// </summary>
+        public const string Name = "AFinType_13";
+
+        /// <summary>
+        /// The apprenticeships (fund model)
+        /// </summary>
+        public const int ApprenticeshipsFundModel = 36;
+
+        /// <summary>
+        /// The programme aim (type)
+        /// </summary>
+        public const int ProgrammeAim = 1;
+
+        /// <summary>
+        /// The total negotiated price (apprenticeship financial type)
+        /// </summary>
+        public const string TotalNegotiatedPrice = "TNP";
+
+        /// <summary>
+        /// The message handler
+        /// </summary>
+        private readonly IValidationErrorHandler _messageHandler;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AFinType_13Rule"/> class.
+        /// </summary>
+        /// <param name="validationErrorHandler">The validation error handler.</param>
+        public AFinType_13Rule(IValidationErrorHandler validationErrorHandler)
+        {
+            It.IsNull(validationErrorHandler)
+                .AsGuard<ArgumentNullException>(nameof(validationErrorHandler));
+
+            _messageHandler = validationErrorHandler;
+        }
+
+        /// <summary>
+        /// Gets the name of the rule.
+        /// </summary>
+        public string RuleName => Name;
+
+        /// <summary>
+        /// Validates the specified object.
+        /// </summary>
+        /// <param name="objectToValidate">The object to validate.</param>
+        public void Validate(ILearner objectToValidate)
+        {
+            It.IsNull(objectToValidate)
+                .AsGuard<ArgumentNullException>(nameof(objectToValidate));
+
+            var learnRefNumber = objectToValidate.LearnRefNumber;
+            var learningDeliveries = objectToValidate.LearningDeliveries;
+            var thisDelivery = learningDeliveries?.FirstOrDefault(d => d.FundModel == ApprenticeshipsFundModel && d.AimType == ProgrammeAim);
+            var thisFinancialRecord = thisDelivery?.AppFinRecords?.FirstOrDefault(afr => afr.AFinType == TotalNegotiatedPrice);
+
+            var failedValidation = !ConditionMet(thisDelivery, thisFinancialRecord);
+
+            if (failedValidation)
+            {
+                RaiseValidationMessage(learnRefNumber, thisDelivery, thisFinancialRecord);
+            }
+        }
+
+        /// <summary>
+        /// Condition met.
+        /// </summary>
+        /// <param name="thisDelivery">this learning delivery.</param>
+        /// <param name="thisFinancialRecord">this financial record.</param>
+        /// <returns>
+        /// true if any any point the conditions are met
+        /// </returns>
+        public bool ConditionMet(ILearningDelivery thisDelivery, IAppFinRecord thisFinancialRecord)
+        {
+            return It.Has(thisDelivery)
+                ? It.Has(thisFinancialRecord)
+                    && thisFinancialRecord.AFinDate > DateTime.MinValue
+                    && thisDelivery.LearnStartDate == thisFinancialRecord.AFinDate
+                : true;
+        }
+
+        /// <summary>
+        /// Raises the validation message.
+        /// </summary>
+        /// <param name="learnRefNumber">The learn reference number.</param>
+        /// <param name="thisDelivery">this learning delivery.</param>
+        /// <param name="thisFinancialRecord">this financial record.</param>
+        public void RaiseValidationMessage(string learnRefNumber, ILearningDelivery thisDelivery, IAppFinRecord thisFinancialRecord)
+        {
+            var parameters = Collection.Empty<IErrorMessageParameter>();
+            parameters.Add(_messageHandler.BuildErrorMessageParameter(MessagePropertyName, thisDelivery));
+            parameters.Add(_messageHandler.BuildErrorMessageParameter(MessagePropertyName, thisFinancialRecord));
+
+            _messageHandler.Handle(RuleName, learnRefNumber, null, parameters);
+        }
+    }
+}
