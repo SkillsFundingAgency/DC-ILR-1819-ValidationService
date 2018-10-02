eb74708aaa33435686bbd3a16c3e07d9aa7a3222
thales-man <msdn@thestripedlawn.co>
2018-10-02 13:32:39 +0100

    #43200 - domicile rule 01

---
+++ src/ESFA.DC.ILR.ValidationService.Rules.Tests/HE/Domicile/Domicile_01RuleTests.cs
@@ -0,0 +1,248 @@
+﻿using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.HE.Domicile;
+using ESFA.DC.ILR.ValidationService.Utility;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Tests.HE.Domicile
+{
+    public class Domicile_01RuleTests
+    {
+        /// <summary>
+        /// New rule with null message handler throws.
+        /// </summary>
+        [Fact]
+        public void NewRuleWithNullMessageHandlerThrows()
+        {
+            // arrange / act / assert
+            Assert.Throws<ArgumentNullException>(() => new Domicile_01Rule(null));
+        }
+
+        /// <summary>
+        /// Rule name 1, matches a literal.
+        /// </summary>
+        [Fact]
+        public void RuleName1()
+        {
+            // arrange
+            var sut = NewRule();
+
+            // act
+            var result = sut.RuleName;
+
+            // assert
+            Assert.Equal("Domicile_01", result);
+        }
+
+        /// <summary>
+        /// Rule name 2, matches the constant.
+        /// </summary>
+        [Fact]
+        public void RuleName2()
+        {
+            // arrange
+            var sut = NewRule();
+
+            // act
+            var result = sut.RuleName;
+
+            // assert
+            Assert.Equal(Domicile_01Rule.Name, result);
+        }
+
+        /// <summary>
+        /// Rule name 3 test, account for potential false positives.
+        /// </summary>
+        [Fact]
+        public void RuleName3()
+        {
+            // arrange
+            var sut = NewRule();
+
+            // act
+            var result = sut.RuleName;
+
+            // assert
+            Assert.NotEqual("SomeOtherRuleName_07", result);
+        }
+
+        /// <summary>
+        /// Validate with null learner throws.
+        /// </summary>
+        [Fact]
+        public void ValidateWithNullLearnerThrows()
+        {
+            // arrange
+            var sut = NewRule();
+
+            // act/assert
+            Assert.Throws<ArgumentNullException>(() => sut.Validate(null));
+        }
+
+        /// <summary>
+        /// Has domicile meets expectation
+        /// </summary>
+        /// <param name="candidate">The candidate.</param>
+        /// <param name="expectation">if set to <c>true</c> [expectation].</param>
+        [Theory]
+        [InlineData("UK-Thingy", true)]
+        [InlineData("!", true)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void HasDomicileMeetsExpectation(string candidate, bool expectation)
+        {
+            // arrange
+            var sut = NewRule();
+            var mockItem = new Mock<ILearningDeliveryHE>();
+            mockItem
+                .SetupGet(y => y.DOMICILE)
+                .Returns(candidate);
+
+            // act
+            var result = sut.HasDomicile(mockItem.Object);
+
+            // assert
+            Assert.Equal(expectation, result);
+        }
+
+        /// <summary>
+        /// Has higher ed with null entity returns false
+        /// </summary>
+        [Fact]
+        public void HasHigherEdWithNullEntityReturnsFalse()
+        {
+            // arrange
+            var sut = NewRule();
+            var mockItem = new Mock<ILearningDelivery>();
+
+            // act
+            var result = sut.HasHigherEd(mockItem.Object);
+
+            // assert
+            Assert.False(result);
+        }
+
+        /// <summary>
+        /// Has higher ed with entity returns true
+        /// </summary>
+        [Fact]
+        public void HasHigherEdWithEntityReturnsTrue()
+        {
+            // arrange
+            var sut = NewRule();
+            var mockItem = new Mock<ILearningDelivery>();
+            mockItem
+                .SetupGet(y => y.LearningDeliveryHEEntity)
+                .Returns(new Mock<ILearningDeliveryHE>().Object);
+
+            // act
+            var result = sut.HasHigherEd(mockItem.Object);
+
+            // assert
+            Assert.True(result);
+        }
+
+        /// <summary>
+        /// Invalid item raises validation message.
+        /// </summary>
+        [Fact]
+        public void InvalidItemRaisesValidationMessage()
+        {
+            // arrange
+            const string LearnRefNumber = "123456789X";
+
+            var mockHE = new Mock<ILearningDeliveryHE>();
+            var mockDelivery = new Mock<ILearningDelivery>();
+            mockDelivery
+                .SetupGet(y => y.LearningDeliveryHEEntity)
+                .Returns(mockHE.Object);
+
+            var deliveries = Collection.Empty<ILearningDelivery>();
+            deliveries.Add(mockDelivery.Object);
+
+            var mockLearner = new Mock<ILearner>();
+            mockLearner
+                .SetupGet(x => x.LearnRefNumber)
+                .Returns(LearnRefNumber);
+            mockLearner
+                .SetupGet(y => y.LearningDeliveries)
+                .Returns(deliveries.AsSafeReadOnlyList());
+
+            var handler = new Mock<IValidationErrorHandler>(MockBehavior.Strict);
+            handler
+                .Setup(x => x.Handle(
+                    Moq.It.Is<string>(y => y == Domicile_01Rule.Name),
+                    Moq.It.Is<string>(y => y == LearnRefNumber),
+                    0,
+                    Moq.It.IsAny<IEnumerable<IErrorMessageParameter>>()));
+            handler
+                .Setup(x => x.BuildErrorMessageParameter(
+                    Moq.It.Is<string>(y => y == Domicile_01Rule.MessagePropertyName),
+                    Moq.It.IsAny<ILearningDelivery>()))
+                .Returns(new Mock<IErrorMessageParameter>().Object);
+
+            var sut = new Domicile_01Rule(handler.Object);
+
+            // act
+            sut.Validate(mockLearner.Object);
+
+            // assert
+            handler.VerifyAll();
+        }
+
+        /// <summary>
+        /// Valid item does not raise validation message.
+        /// </summary>
+        [Fact]
+        public void ValidItemDoesNotRaiseValidationMessage()
+        {
+            // arrange
+            const string LearnRefNumber = "123456789X";
+
+            var mockHE = new Mock<ILearningDeliveryHE>();
+            mockHE
+                .SetupGet(x => x.DOMICILE)
+                .Returns("i am a domicile");
+
+            var mockDelivery = new Mock<ILearningDelivery>();
+            mockDelivery
+                .SetupGet(y => y.LearningDeliveryHEEntity)
+                .Returns(mockHE.Object);
+
+            var deliveries = Collection.Empty<ILearningDelivery>();
+            deliveries.Add(mockDelivery.Object);
+
+            var mockLearner = new Mock<ILearner>();
+            mockLearner
+                .SetupGet(x => x.LearnRefNumber)
+                .Returns(LearnRefNumber);
+            mockLearner
+                .SetupGet(y => y.LearningDeliveries)
+                .Returns(deliveries.AsSafeReadOnlyList());
+
+            var handler = new Mock<IValidationErrorHandler>(MockBehavior.Strict);
+
+            var sut = new Domicile_01Rule(handler.Object);
+
+            // act
+            sut.Validate(mockLearner.Object);
+
+            // assert
+            handler.VerifyAll();
+        }
+
+        /// <summary>
+        /// New rule.
+        /// </summary>
+        /// <returns>a constructed and mocked up validation rule</returns>
+        public Domicile_01Rule NewRule()
+        {
+            var handler = new Mock<IValidationErrorHandler>(MockBehavior.Strict);
+
+            return new Domicile_01Rule(handler.Object);
+        }
+    }
+}
