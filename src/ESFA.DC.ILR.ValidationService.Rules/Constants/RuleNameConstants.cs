caa141a3b9b022fe5eca88725cea92d5929d3d03
saqib2013 <5689652+saqib2013@users.noreply.github.com>
2018-10-15 16:47:11 +0100

    44157 - LearnDelFAMType_08 validation rule implementation.

--- src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
@@ -129,6 +129,7 @@
 
         public const string LearnDelFAMType_02 = "LearnDelFAMType_02";
         public const string LearnDelFAMType_03 = "LearnDelFAMType_03";
+        public const string LearnDelFAMType_08 = "LearnDelFAMType_08";
         public const string LearnDelFAMType_39 = "LearnDelFAMType_39";
         public const string LearnDelFAMType_44 = "LearnDelFAMType_44";
         public const string LearnDelFAMType_64 = "LearnDelFAMType_64";
