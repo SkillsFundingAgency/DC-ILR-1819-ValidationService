b42a5bd55b9a0ab65ec09c5c6dc3e682b79e58bb
Cory-Wynn <CoryWynn1993@gmail.com>
2018-10-17 09:58:15 +0100

    NINumber_01 Rule Implementation

--- src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
@@ -143,6 +143,9 @@
 
         public const string NETFEE_01 = "NETFEE_01";
         public const string NETFEE_02 = "NETFEE_02";
+
+        public const string NINumber_01 = "NiNumber_01";
+
         public const string NUMHUS_01 = "NUMHUS_01";
 
         public const string OtherFundAdj_01 = "OtherFundAdj_01";
