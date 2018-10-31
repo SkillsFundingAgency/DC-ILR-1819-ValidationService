9e6b3ed11e0d116844aafcd2190393ab30f31b25
thales-man <msdn@thestripedlawn.co>
2018-10-31 14:16:55 +0000

    a bit more of the last one...

--- src/ESFA.DC.ILR.ValidationService.RuleSet.Modules.Tests/MessageRuleSetModuleTests.cs
+++ src/ESFA.DC.ILR.ValidationService.RuleSet.Modules.Tests/MessageRuleSetModuleTests.cs
@@ -1,24 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Autofac;
+﻿using Autofac;
 using ESFA.DC.ILR.Model.Interface;
 using ESFA.DC.ILR.ValidationService.Interface;
 using ESFA.DC.ILR.ValidationService.Modules;
-using ESFA.DC.ILR.ValidationService.Rules.CrossEntity;
-using ESFA.DC.ILR.ValidationService.Rules.Message.FileLevel.Entity;
-using ESFA.DC.ILR.ValidationService.Rules.Message.FileLevel.Header;
-using ESFA.DC.ILR.ValidationService.Rules.Message.UKPRN;
-using FluentAssertions;
 using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using Xunit;
 
 namespace ESFA.DC.ILR.ValidationService.RuleSet.Modules.Tests
 {
     public class MessageRuleSetModuleTests
     {
+        /// <summary>
+        /// Assembly and registration rule cardinality is correct.
+        /// </summary>
         [Fact]
-        public void MessageRuleSet()
+        public void AssemblyAndRegistrationRuleCardinalityIsCorrect()
+        {
+            // arrange
+            var registeredItems = GetContainerRuleSet();
+            var assemblyItems = GetAssemblyRuleSet();
+
+            var issues = new List<string>();
+
+            // act
+            foreach (var ruleType in assemblyItems)
+            {
+                if (registeredItems.Count(x => x.GetType().Name == ruleType.Name) != 1)
+                {
+                    issues.Add(ruleType.Name);
+                }
+            }
+
+            // assert
+            Assert.Empty(issues);
+        }
+
+        /// <summary>
+        /// Assembly and the registration counts match.
+        /// </summary>
+        [Fact]
+        public void AssemblyAndRegistrationCountsMatch()
+        {
+            // arrange
+            var registeredItems = GetContainerRuleSet();
+            var assemblyItems = GetAssemblyRuleSet();
+
+            // act
+            var registeredCount = registeredItems.Count();
+            var assemblyCount = assemblyItems.Count();
+
+            // assert
+            Assert.Equal(registeredCount, assemblyCount);
+        }
+
+        /// <summary>
+        /// Gets the assembly rule set.
+        /// </summary>
+        /// <returns>a collection of <see cref="Type"/></returns>
+        public IReadOnlyCollection<Type> GetAssemblyRuleSet()
+        {
+            var asm = Assembly.LoadFile(Path.Combine(Environment.CurrentDirectory, "ESFA.DC.ILR.ValidationService.Rules.dll"));
+            return asm.GetTypes()
+                .Where(x => typeof(IRule<IMessage>).IsAssignableFrom(x) && !x.IsAbstract)
+                .ToList();
+        }
+
+        /// <summary>
+        /// Gets the container rule set.
+        /// </summary>
+        /// <returns>a collection of <see cref="IRule{ILearner}"/></returns>
+        public IReadOnlyCollection<IRule<IMessage>> GetContainerRuleSet()
         {
             var builder = new ContainerBuilder();
 
@@ -29,27 +84,7 @@ namespace ESFA.DC.ILR.ValidationService.RuleSet.Modules.Tests
 
             var container = builder.Build();
 
-            var rules = container.Resolve<IEnumerable<IRule<IMessage>>>().ToList();
-
-            rules.Should().ContainItemsAssignableTo<IRule<IMessage>>();
-
-            var ruleTypes = new List<Type>()
-            {
-                typeof(Entity_1Rule),
-                typeof(Header_2Rule),
-                typeof(Header_3Rule),
-                typeof(UKPRN_03Rule),
-                typeof(R06Rule),
-                typeof(R85Rule),
-                typeof(R108Rule),
-            };
-
-            foreach (var ruleType in ruleTypes)
-            {
-                rules.Should().ContainSingle(r => r.GetType() == ruleType);
-            }
-
-            rules.Should().HaveCount(7);
+            return container.Resolve<IEnumerable<IRule<IMessage>>>().ToList();
         }
 
         private void RegisterDependencies(ContainerBuilder builder)
