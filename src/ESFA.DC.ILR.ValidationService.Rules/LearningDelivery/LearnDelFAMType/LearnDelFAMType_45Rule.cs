b52144e60ff417a012cde529292245cf5eb4b405
saqib2013 <5689652+saqib2013@users.noreply.github.com>
2018-11-07 16:30:41 +0000

    44247 - LearnDelFAMType_45 validation rule implemented and unit tests added.

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/LearnDelFAMType/LearnDelFAMType_45Rule.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+using ESFA.DC.ILR.ValidationService.Rules.Query.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.LearnDelFAMType
+{
+    public class LearnDelFAMType_45Rule : AbstractRule, IRule<ILearner>
+    {
+        private readonly ILearningDeliveryFAMQueryService _learningDeliveryFAMQueryService;
+
+        public LearnDelFAMType_45Rule(
+            IValidationErrorHandler validationErrorHandler,
+            ILearningDeliveryFAMQueryService learningDeliveryFAMQueryService)
+            : base(validationErrorHandler, RuleNameConstants.LearnDelFAMType_45)
+        {
+            _learningDeliveryFAMQueryService = learningDeliveryFAMQueryService;
+        }
+
+        public void Validate(ILearner objectToValidate)
+        {
+            if (objectToValidate == null)
+            {
+                return;
+            }
+
+            foreach (var learningDelivery in objectToValidate.LearningDeliveries)
+            {
+                if (ConditionMet(learningDelivery.LearningDeliveryFAMs))
+                {
+                    HandleValidationError(objectToValidate.LearnRefNumber, learningDelivery.AimSeqNumber, BuildErrorMessageParameters(LearningDeliveryFAMTypeConstants.HHS));
+                }
+            }
+        }
+
+        public bool ConditionMet(IReadOnlyCollection<ILearningDeliveryFAM> learningDeliveryFAMs)
+        {
+            return _learningDeliveryFAMQueryService.GetLearningDeliveryFAMsCountByFAMType(learningDeliveryFAMs, LearningDeliveryFAMTypeConstants.HHS) > 2;
+        }
+
+        public IEnumerable<IErrorMessageParameter> BuildErrorMessageParameters(string learnDelFAMType)
+        {
+            return new[]
+            {
+                BuildErrorMessageParameter(PropertyNameConstants.LearnDelFAMType, learnDelFAMType)
+            };
+        }
+    }
+}
