13667efc5aa8f35aa660cb66f86bd0aea250b155
thales-man <msdn@thestripedlawn.co>
2018-11-11 16:08:38 +0000

    #59379 - emp stat rule 14

--- src/ESFA.DC.ILR.ValidationService.Rules.Tests/EmploymentStatus/EmpStat/EmpStat_14RuleTests.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules.Tests/EmploymentStatus/EmpStat/EmpStat_14RuleTests.cs
@@ -251,138 +251,44 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.EmploymentStatus.EmpStat
             Assert.Null(result);
         }
 
-        /*
         [Theory]
-        [InlineData(TypeOfEmploymentStatus.InPaidEmployment, false)]
-        [InlineData(TypeOfEmploymentStatus.NotEmployedNotSeekingOrNotAvailable, false)]
-        [InlineData(TypeOfEmploymentStatus.NotEmployedSeekingAndAvailable, false)]
-        [InlineData(TypeOfEmploymentStatus.NotKnownProvided, true)]
-        public void IsNotKnownOrProvidedMeetsExpectation(int candidate, bool expectation)
+        [InlineData("2018-09-11", "2014-08-01", "2018-09-11", "2016-02-11", "2017-06-09")]
+        [InlineData("2017-12-31", "2015-12-31", "2017-12-30", "2014-12-31", "2017-10-16")]
+        [InlineData("2018-07-01", "2018-06-30", "2014-05-11", "2014-07-12")]
+        [InlineData("2016-11-17", "2016-11-17")]
+        public void GetClosestEmploymentMeetsExpectation(string candidate, params string[] d22Dates)
         {
             // arrange
-            var sut = NewRule();
-            var mockItem = new Mock<ILearnerEmploymentStatus>();
-            mockItem
-                .SetupGet(y => y.EmpStat)
-                .Returns(candidate);
+            var testDate = DateTime.Parse(candidate);
 
-            // act
-            var result = sut.IsNotKnownOrProvided(mockItem.Object);
+            var statusCandidates = Collection.Empty<DateTime>();
+            d22Dates.ForEach(x => statusCandidates.Add(DateTime.Parse(x)));
+            var expectedDate = statusCandidates.Max();
 
-            // assert
-            Assert.Equal(expectation, result);
-        }
-
-        [Fact]
-        public void HasQualifyingCompletedContractWithNullDeliveriesReturnsFalse()
-        {
-            // arrange
-            var sut = NewRule();
-            var mockItem = new Mock<ILearner>();
-
-            // act
-            var result = sut.HasQualifyingCompletedContract(mockItem.Object, DateTime.Today);
-
-            // assert
-            Assert.False(result);
-        }
-
-        [Fact]
-        public void HasQualifyingCompletedContractWithEmptyDeliveriesReturnsFalse()
-        {
-            // arrange
-            var sut = NewRule();
-            var mockItem = new Mock<ILearner>();
-            mockItem
-                .SetupGet(x => x.LearningDeliveries)
-                .Returns(Collection.EmptyAndReadOnly<ILearningDelivery>());
-
-            // act
-            var result = sut.HasQualifyingCompletedContract(mockItem.Object, DateTime.Today);
-
-            // assert
-            Assert.False(result);
-        }
-
-        [Theory]
-        [InlineData("2018-03-12", null, false)]
-        [InlineData("2018-03-12", "2018-03-10", false)]
-        [InlineData("2018-03-12", "2018-03-11", false)]
-        [InlineData("2018-03-12", "2018-03-12", true)]
-        [InlineData("2018-03-12", "2018-03-13", false)]
-        public void HasQualifyingCompletedContractMeetsExpectation(string inDate, string outDate, bool expectation)
-        {
-            // arrange
-            var empStatDate = DateTime.Parse(inDate);
-            var learnStartDate = !string.IsNullOrWhiteSpace(outDate)
-                ? DateTime.Parse(outDate)
-                : (DateTime?)null;
-
-            var handler = new Mock<IValidationErrorHandler>(MockBehavior.Strict);
-            var mockDDRule22 = new Mock<IDerivedData_22Rule>(MockBehavior.Strict);
-            mockDDRule22
-                .Setup(x => x.GetLatestLearningStartForESFContract(Moq.It.IsAny<ILearningDelivery>(), Moq.It.IsAny<IReadOnlyCollection<ILearningDelivery>>()))
-                .Returns(learnStartDate);
-
-            var sut = new EmpStat_14Rule(handler.Object, mockDDRule22.Object);
-
-            var deliveries = Collection.Empty<ILearningDelivery>();
-            if (learnStartDate.HasValue)
-            {
-                var del = new Mock<ILearningDelivery>();
-                del
-                    .SetupGet(x => x.LearnStartDate)
-                    .Returns(learnStartDate.Value);
-
-                deliveries.Add(del.Object);
-            }
-
-            var mockItem = new Mock<ILearner>();
-            mockItem
-                .SetupGet(x => x.LearningDeliveries)
-                .Returns(deliveries.AsSafeReadOnlyList());
-
-            // act
-            var result = sut.HasQualifyingCompletedContract(mockItem.Object, empStatDate);
-
-            // assert
-            Assert.Equal(expectation, result);
-        }
-
-        /// <summary>
-        /// Get latest learning start for esf contract returns expected date.
-        /// </summary>
-        /// <param name="outDate">The out date.</param>
-        [Theory]
-        [InlineData(null)]
-        [InlineData("2018-03-10")]
-        [InlineData("2018-03-11")]
-        [InlineData("2018-03-12")]
-        [InlineData("2018-03-13")]
-        public void GetLatestLearningStartForESFContractReturnsExpectedDate(string outDate)
-        {
-            // arrange
-            var learnStartDate = !string.IsNullOrWhiteSpace(outDate)
-                ? DateTime.Parse(outDate)
-                : (DateTime?)null;
-
-            var deliveries = Collection.EmptyAndReadOnly<ILearningDelivery>();
-            var mockDel = new Mock<ILearningDelivery>();
+            var employmentCandidates = Collection.Empty<ILearnerEmploymentStatus>();
+            statusCandidates
+                .ForEach(x =>
+                {
+                    var mockItem = new Mock<ILearnerEmploymentStatus>();
+                    mockItem.SetupGet(y => y.DateEmpStatApp).Returns(x);
+                    employmentCandidates.Add(mockItem.Object);
+                });
 
             var handler = new Mock<IValidationErrorHandler>(MockBehavior.Strict);
             var ddRule22 = new Mock<IDerivedData_22Rule>(MockBehavior.Strict);
-            ddRule22
-                .Setup(x => x.GetLatestLearningStartForESFContract(mockDel.Object, deliveries))
-                .Returns(learnStartDate);
             var fcsData = new Mock<IFCSDataService>(MockBehavior.Strict);
 
             var sut = new EmpStat_14Rule(handler.Object, ddRule22.Object, fcsData.Object);
 
             // act
-            var result = sut.get(mockDel.Object, deliveries);
+            var result = sut.GetClosestEmployment(employmentCandidates.AsSafeReadOnlyList(), testDate);
 
             // assert
-            Assert.Equal(learnStartDate, result);
+            handler.VerifyAll();
+            ddRule22.VerifyAll();
+            fcsData.VerifyAll();
+
+            Assert.Equal(expectedDate, result.DateEmpStatApp);
         }
 
         /// <summary>
@@ -392,26 +298,45 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.EmploymentStatus.EmpStat
         /// <param name="d22Dates">The D22 dates.</param>
         [Theory]
         [InlineData("2018-09-11", "2014-08-01", "2018-09-11", null, "2016-02-11", null, "2017-06-09")]
-        [InlineData("2017-12-31", null, "2015-12-31", "2017-12-30", "2014-12-31", null, "2017-10-16", null)]
-        [InlineData("2018-07-01", "2018-06-30", "2014-05-11", "2014-07-12")]
+        [InlineData("2017-12-31", null, "2015-12-31", "2017-12-31", "2017-12-30", "2014-12-31", null, "2017-10-16", null)]
+        [InlineData("2018-07-01", "2018-06-30", "2018-07-01", "2014-05-11", "2014-07-12")]
         [InlineData("2016-11-17", "2016-11-17", null)]
         public void InvalidItemRaisesValidationMessage(string candidate, params string[] d22Dates)
         {
             // arrange
             const string LearnRefNumber = "123456789X";
+            const string conRefNumber = "test-Con-Ref";
 
             var testDate = DateTime.Parse(candidate);
 
-            var latestContractCandidates = Collection.Empty<DateTime?>();
-            d22Dates.ForEach(x => latestContractCandidates.Add(GetNullableDate(x)));
-            var expectedContractDate = latestContractCandidates.Max();
+            var contractCandidates = Collection.Empty<DateTime?>();
+            d22Dates.ForEach(x => contractCandidates.Add(GetNullableDate(x)));
 
             var deliveries = Collection.Empty<ILearningDelivery>();
-            for (int i = 0; i < latestContractCandidates.Count; i++)
+            for (int i = 0; i < contractCandidates.Count - 1; i++)
             {
                 var mockDelivery = new Mock<ILearningDelivery>();
                 deliveries.Add(mockDelivery.Object);
             }
+
+            var mockItem = new Mock<ILearningDelivery>();
+            mockItem
+                .SetupGet(x => x.LearnAimRef)
+                .Returns(TypeOfAim.References.ESFLearnerStartandAssessment);
+            mockItem
+                .SetupGet(x => x.CompStatus)
+                .Returns(CompletionState.HasCompleted);
+            mockItem
+                .SetupGet(x => x.FundModel)
+                .Returns(TypeOfFunding.EuropeanSocialFund);
+            mockItem
+                .SetupGet(x => x.LearnStartDate)
+                .Returns(testDate);
+            mockItem
+                .SetupGet(x => x.ConRefNumber)
+                .Returns(conRefNumber);
+
+            deliveries.Add(mockItem.Object);
 
             var safeDeliveries = deliveries.AsSafeReadOnlyList();
 
@@ -419,6 +344,9 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.EmploymentStatus.EmpStat
             mockStatus
                 .SetupGet(y => y.DateEmpStatApp)
                 .Returns(testDate);
+            mockStatus
+                .SetupGet(y => y.EmpStat)
+                .Returns(TypeOfEmploymentStatus.InPaidEmployment);
 
             var statii = Collection.Empty<ILearnerEmploymentStatus>();
             statii.Add(mockStatus.Object);
@@ -439,20 +367,38 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.EmploymentStatus.EmpStat
                 .Setup(x => x.Handle(
                     Moq.It.Is<string>(y => y == EmpStat_14Rule.Name),
                     Moq.It.Is<string>(y => y == LearnRefNumber),
-                    null,
+                    0,
                     Moq.It.IsAny<IEnumerable<IErrorMessageParameter>>()));
             handler
                 .Setup(x => x.BuildErrorMessageParameter(
                     Moq.It.Is<string>(y => y == EmpStat_14Rule.MessagePropertyName),
-                    TypeOfEmploymentStatus.NotKnownProvided))
+                    TypeOfEmploymentStatus.InPaidEmployment))
+                .Returns(new Mock<IErrorMessageParameter>().Object);
+            handler
+                .Setup(x => x.BuildErrorMessageParameter(
+                    Moq.It.Is<string>(y => y == PropertyNameConstants.ConRefNumber),
+                    conRefNumber))
+                .Returns(new Mock<IErrorMessageParameter>().Object);
+            handler
+                .Setup(x => x.BuildErrorMessageParameter(
+                    Moq.It.Is<string>(y => y == PropertyNameConstants.LearnStartDate),
+                    testDate))
                 .Returns(new Mock<IErrorMessageParameter>().Object);
 
             var ddRule22 = new Mock<IDerivedData_22Rule>(MockBehavior.Strict);
             ddRule22
                 .Setup(x => x.GetLatestLearningStartForESFContract(Moq.It.IsAny<ILearningDelivery>(), safeDeliveries))
-                .ReturnsInOrder(latestContractCandidates);
+                .ReturnsInOrder(contractCandidates);
+
+            var eligibubble = new Mock<IEsfEligibilityRuleEmploymentStatus>();
+            eligibubble
+                .SetupGet(x => x.Code)
+                .Returns(TypeOfEmploymentStatus.NotEmployedNotSeekingOrNotAvailable);
 
             var fcsData = new Mock<IFCSDataService>(MockBehavior.Strict);
+            fcsData
+                .Setup(x => x.GetEligibilityRuleEmploymentStatus(conRefNumber))
+                .Returns(eligibubble.Object);
 
             var sut = new EmpStat_14Rule(handler.Object, ddRule22.Object, fcsData.Object);
 
@@ -468,28 +414,49 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.EmploymentStatus.EmpStat
         /// <summary>
         /// Valid item does not raise validation message.
         /// </summary>
+        /// <param name="candidate">The candidate.</param>
+        /// <param name="d22Dates">The D22 dates.</param>
         [Theory]
-        [InlineData("2018-09-11", "2014-08-01", "2018-09-12", null, "2016-02-11", null, "2017-06-09")]
-        [InlineData("2017-12-31", null, "2015-12-31", "2018-01-01", "2014-12-31", null, "2017-10-16", null)]
-        [InlineData("2018-07-01", "2018-07-02", "2014-05-11", "2014-07-12")]
-        [InlineData("2016-11-17", "2016-11-18", null)]
+        [InlineData("2018-09-11", "2014-08-01", "2018-09-11", null, "2016-02-11", null, "2017-06-09")]
+        [InlineData("2017-12-31", null, "2015-12-31", "2017-12-31", "2017-12-30", "2014-12-31", null, "2017-10-16", null)]
+        [InlineData("2018-07-01", "2018-06-30", "2018-07-01", "2014-05-11", "2014-07-12")]
+        [InlineData("2016-11-17", "2016-11-17", null)]
         public void ValidItemDoesNotRaiseValidationMessage(string candidate, params string[] d22Dates)
         {
             // arrange
             const string LearnRefNumber = "123456789X";
+            const string conRefNumber = "test-Con-Ref";
 
             var testDate = DateTime.Parse(candidate);
 
-            var latestContractCandidates = Collection.Empty<DateTime?>();
-            d22Dates.ForEach(x => latestContractCandidates.Add(GetNullableDate(x)));
-            var expectedContractDate = latestContractCandidates.Max();
+            var contractCandidates = Collection.Empty<DateTime?>();
+            d22Dates.ForEach(x => contractCandidates.Add(GetNullableDate(x)));
 
             var deliveries = Collection.Empty<ILearningDelivery>();
-            for (int i = 0; i < latestContractCandidates.Count; i++)
+            for (int i = 0; i < contractCandidates.Count - 1; i++)
             {
                 var mockDelivery = new Mock<ILearningDelivery>();
                 deliveries.Add(mockDelivery.Object);
             }
+
+            var mockItem = new Mock<ILearningDelivery>();
+            mockItem
+                .SetupGet(x => x.LearnAimRef)
+                .Returns(TypeOfAim.References.ESFLearnerStartandAssessment);
+            mockItem
+                .SetupGet(x => x.CompStatus)
+                .Returns(CompletionState.HasCompleted);
+            mockItem
+                .SetupGet(x => x.FundModel)
+                .Returns(TypeOfFunding.EuropeanSocialFund);
+            mockItem
+                .SetupGet(x => x.LearnStartDate)
+                .Returns(testDate);
+            mockItem
+                .SetupGet(x => x.ConRefNumber)
+                .Returns(conRefNumber);
+
+            deliveries.Add(mockItem.Object);
 
             var safeDeliveries = deliveries.AsSafeReadOnlyList();
 
@@ -497,6 +464,9 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.EmploymentStatus.EmpStat
             mockStatus
                 .SetupGet(y => y.DateEmpStatApp)
                 .Returns(testDate);
+            mockStatus
+                .SetupGet(y => y.EmpStat)
+                .Returns(TypeOfEmploymentStatus.InPaidEmployment);
 
             var statii = Collection.Empty<ILearnerEmploymentStatus>();
             statii.Add(mockStatus.Object);
@@ -516,9 +486,17 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.EmploymentStatus.EmpStat
             var ddRule22 = new Mock<IDerivedData_22Rule>(MockBehavior.Strict);
             ddRule22
                 .Setup(x => x.GetLatestLearningStartForESFContract(Moq.It.IsAny<ILearningDelivery>(), safeDeliveries))
-                .ReturnsInOrder(latestContractCandidates);
+                .ReturnsInOrder(contractCandidates);
+
+            var eligibubble = new Mock<IEsfEligibilityRuleEmploymentStatus>();
+            eligibubble
+                .SetupGet(x => x.Code)
+                .Returns(TypeOfEmploymentStatus.InPaidEmployment);
 
             var fcsData = new Mock<IFCSDataService>(MockBehavior.Strict);
+            fcsData
+                .Setup(x => x.GetEligibilityRuleEmploymentStatus(conRefNumber))
+                .Returns(eligibubble.Object);
 
             var sut = new EmpStat_14Rule(handler.Object, ddRule22.Object, fcsData.Object);
 
@@ -530,7 +508,6 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.EmploymentStatus.EmpStat
             ddRule22.VerifyAll();
             fcsData.VerifyAll();
         }
-        */
 
         /// <summary>
         /// Gets a nullable date.
