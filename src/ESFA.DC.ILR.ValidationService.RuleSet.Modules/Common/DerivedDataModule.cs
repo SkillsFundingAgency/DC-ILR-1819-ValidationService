3a8beccfec2792f817f4afe6f0a7139f37e833e1
saqib2013 <5689652+saqib2013@users.noreply.github.com>
2018-11-12 15:14:16 +0000

    42433 - DerivedData27 validation rule implemented and unit tests added.

--- src/ESFA.DC.ILR.ValidationService.RuleSet.Modules/Common/DerivedDataModule.cs
+++ src/ESFA.DC.ILR.ValidationService.RuleSet.Modules/Common/DerivedDataModule.cs
@@ -15,6 +15,7 @@ namespace ESFA.DC.ILR.ValidationService.RuleSet.Modules.Common
             builder.RegisterType<DerivedData_11Rule>().As<IDerivedData_11Rule>().InstancePerLifetimeScope();
             builder.RegisterType<DerivedData_21Rule>().As<IDerivedData_21Rule>().InstancePerLifetimeScope();
             builder.RegisterType<DerivedData_22Rule>().As<IDerivedData_22Rule>().InstancePerLifetimeScope();
+            builder.RegisterType<DerivedData_27Rule>().As<IDerivedData_27Rule>().InstancePerLifetimeScope();
             builder.RegisterType<DerivedData_28Rule>().As<IDerivedData_28Rule>().InstancePerLifetimeScope();
             builder.RegisterType<DerivedData_29Rule>().As<IDerivedData_29Rule>().InstancePerLifetimeScope();
         }
