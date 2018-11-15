f62ec6ca6bf6e99d171ccf3bfbf99209d7d951da
saqib2013 <5689652+saqib2013@users.noreply.github.com>
2018-11-15 13:21:31 +0000

    42980 - LearningDeliveryHE_03 rule missed files checked in.

--- src/ESFA.DC.ILR.ValidationService.Rules/HE/LearningDeliveryHE/LearningDeliveryHE_03Rule.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/HE/LearningDeliveryHE/LearningDeliveryHE_03Rule.cs
@@ -5,10 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using ESFA.DC.ILR.Model.Interface;
 using ESFA.DC.ILR.ValidationService.Data.External.LARS.Interface;
+using ESFA.DC.ILR.ValidationService.Data.External.Organisation.Interface;
 using ESFA.DC.ILR.ValidationService.Interface;
 using ESFA.DC.ILR.ValidationService.Rules.Abstract;
 using ESFA.DC.ILR.ValidationService.Rules.Constants;
 using ESFA.DC.ILR.ValidationService.Rules.Derived.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Query.Interface;
 
 namespace ESFA.DC.ILR.ValidationService.Rules.HE.LearningDeliveryHE
 {
@@ -34,14 +36,20 @@ namespace ESFA.DC.ILR.ValidationService.Rules.HE.LearningDeliveryHE
         private readonly IDD07 _dd07;
         private readonly ILARSDataService _lARSDataService;
         private readonly IDerivedData_27Rule _derivedData_27Rule;
+        private readonly IOrganisationDataService _organisationDataService;
+        private readonly ILearningDeliveryFAMQueryService _learningDeliveryFAMQueryService;
 
         public LearningDeliveryHE_03Rule(
             IValidationErrorHandler validationErrorHandler,
+            ILearningDeliveryFAMQueryService learningDeliveryFAMQueryService,
+            IOrganisationDataService organisationDataService,
             IDerivedData_27Rule derivedData_27Rule,
             ILARSDataService lARSDataService,
             IDD07 dd07)
             : base(validationErrorHandler, RuleNameConstants.LearningDeliveryHE_03)
         {
+            _learningDeliveryFAMQueryService = learningDeliveryFAMQueryService;
+            _organisationDataService = organisationDataService;
             _derivedData_27Rule = derivedData_27Rule;
             _lARSDataService = lARSDataService;
             _dd07 = dd07;
@@ -50,7 +58,8 @@ namespace ESFA.DC.ILR.ValidationService.Rules.HE.LearningDeliveryHE
         public void Validate(IMessage objectToValidate)
         {
             if (objectToValidate == null
-                    || DerivedData27ConditionMet(objectToValidate.LearningProviderEntity.UKPRN))
+                    || _organisationDataService.LegalOrgTypeMatchForUkprn(objectToValidate.LearningProviderEntity.UKPRN, LegalOrgTypeConstants.UHEO)
+                    || !DerivedData27ConditionMet(objectToValidate.LearningProviderEntity.UKPRN))
             {
                 return;
             }
@@ -59,7 +68,12 @@ namespace ESFA.DC.ILR.ValidationService.Rules.HE.LearningDeliveryHE
             {
                 foreach (var learningDelivery in learner.LearningDeliveries)
                 {
-                    if (ConditionMet(learningDelivery.FundModel, learningDelivery.LearnAimRef))
+                    if (ConditionMet(
+                        learningDelivery.FundModel,
+                        learningDelivery.LearnAimRef,
+                        learningDelivery.ProgTypeNullable,
+                        learningDelivery.LearningDeliveryHEEntity,
+                        learningDelivery.LearningDeliveryFAMs))
                     {
                         HandleValidationError(learner.LearnRefNumber, learningDelivery.AimSeqNumber, BuildErrorMessageParameters(learningDelivery.FundModel));
                     }
@@ -67,11 +81,26 @@ namespace ESFA.DC.ILR.ValidationService.Rules.HE.LearningDeliveryHE
             }
         }
 
-        public bool ConditionMet(int fundModel, string learnAimRef)
+        public bool ConditionMet(
+            int fundModel,
+            string learnAimRef,
+            int? progTypeNullable,
+            ILearningDeliveryHE learningDeliveryHEEntity,
+            IReadOnlyCollection<ILearningDeliveryFAM> learningDeliveryFAMs)
         {
             return FundModelConditionMet(fundModel)
-                && LARSNotionalNVQLevelV2ConditionMet(learnAimRef);
+                && LARSNotionalNVQLevelV2ConditionMet(learnAimRef)
+                && LearningDeliveryHEConditionMet(learningDeliveryHEEntity)
+                && DD07ConditionMet(progTypeNullable)
+                && LearningDeliveryFAMsCondtionMet(learningDeliveryFAMs);
         }
+
+        public bool LearningDeliveryFAMsCondtionMet(IReadOnlyCollection<ILearningDeliveryFAM> learningDeliveryFAMs)
+            => !_learningDeliveryFAMQueryService.HasLearningDeliveryFAMCodeForType(learningDeliveryFAMs, LearningDeliveryFAMTypeConstants.LDM, "352");
+
+        public bool DD07ConditionMet(int? progType) => !_dd07.IsApprenticeship(progType);
+
+        public bool LearningDeliveryHEConditionMet(ILearningDeliveryHE learningDeliveryHEEntity) => learningDeliveryHEEntity == null;
 
         public bool LARSNotionalNVQLevelV2ConditionMet(string learnAimRef) => _lARSDataService.NotionalNVQLevelV2MatchForLearnAimRefAndLevels(learnAimRef, _notionalNVQLevels);
 
