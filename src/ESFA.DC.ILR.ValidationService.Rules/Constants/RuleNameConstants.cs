8a35afc23dc556a3875b37828eee420683c15745
Cory-Wynn <CoryWynn1993@gmail.com>
2018-11-20 14:24:08 +0000

    42974 LearningDeliveryHE_02 rule and tests

--- src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
@@ -154,6 +154,7 @@
         public const string LearnDelFAMType_64 = "LearnDelFAMType_64";
         public const string LearnDelFAMType_71 = "LearnDelFAMType_71";
 
+        public const string LearningDeliveryHE_02 = "LearningDeliveryHE_02";
         public const string LearningDeliveryHE_03 = "LearningDeliveryHE_03";
 
         public const string LearnFAMType_16 = "LearnFAMType_16";
