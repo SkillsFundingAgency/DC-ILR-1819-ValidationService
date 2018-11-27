69554bbab6b50400b3a1ee6ded308fbbbef3c070
Cory-Wynn <CoryWynn1993@gmail.com>
2018-11-27 09:36:54 +0000

    43576 LearnStartDate_03 rule implementation

--- src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/LearnStartDate/LearnStartDate_03Rule.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/LearnStartDate/LearnStartDate_03Rule.cs
@@ -1,41 +1,71 @@
-﻿using System;
-using ESFA.DC.ILR.Model.Interface;
+﻿using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Data.Internal.AcademicYear.Interface;
 using ESFA.DC.ILR.ValidationService.Interface;
 using ESFA.DC.ILR.ValidationService.Rules.Abstract;
 using ESFA.DC.ILR.ValidationService.Rules.Constants;
 using ESFA.DC.ILR.ValidationService.Rules.Derived.Interface;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.LearnStartDate
 {
     public class LearnStartDate_03Rule : AbstractRule, IRule<ILearner>
     {
         private readonly IDD07 _dd07;
-        private readonly IValidationDataService _validationDataService;
+        private readonly IAcademicYearDataService _academicYearDataService;
 
-        public LearnStartDate_03Rule(IDD07 dd07, IValidationDataService validationDataService, IValidationErrorHandler validationErrorHandler)
-            : base(validationErrorHandler)
+        public LearnStartDate_03Rule(
+            IDD07 dd07,
+            IAcademicYearDataService academicYearDataService,
+            IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler, RuleNameConstants.LearnStartDate_03)
         {
             _dd07 = dd07;
-            _validationDataService = validationDataService;
+            _academicYearDataService = academicYearDataService;
         }
 
         public void Validate(ILearner objectToValidate)
         {
             foreach (var learningDelivery in objectToValidate.LearningDeliveries)
             {
-                if (ConditionMet(learningDelivery.LearnStartDate, _validationDataService.AcademicYearEnd, learningDelivery.ProgTypeNullable, _dd07.Derive(learningDelivery.ProgTypeNullable)))
+                if (ConditionMet(
+                    learningDelivery.LearnStartDate,
+                    learningDelivery.ProgTypeNullable))
                 {
-                    HandleValidationError(RuleNameConstants.LearnStartDate_03, objectToValidate.LearnRefNumber, learningDelivery.AimSeqNumber);
+                    HandleValidationError(
+                        objectToValidate.LearnRefNumber,
+                        learningDelivery.AimSeqNumber,
+                        BuildErrorMessageParameters(learningDelivery.LearnStartDate));
                 }
             }
         }
 
-        public bool ConditionMet(DateTime learnStartDate, DateTime academicYearEnd, long? progType, string dd07)
+        public bool ConditionMet(DateTime learnStartDate, int? progType)
         {
-            return dd07 == ValidationConstants.N
-                && progType != 24
-                && learnStartDate.HasValue
-                && learnStartDate > academicYearEnd;
+            return DD07ConditionMet(progType)
+                   && LearnStartDateConditionMet(learnStartDate);
+        }
+
+        public bool DD07ConditionMet(int? progType)
+        {
+            return progType != 24
+                   && !_dd07.IsApprenticeship(progType);
+        }
+
+        public bool LearnStartDateConditionMet(DateTime learnStartDate)
+        {
+            var academicYearEnd = _academicYearDataService.End();
+
+            return learnStartDate > academicYearEnd;
+        }
+
+        public IEnumerable<IErrorMessageParameter> BuildErrorMessageParameters(DateTime learnStartDate)
+        {
+            return new[]
+            {
+                BuildErrorMessageParameter(PropertyNameConstants.LearnStartDate, learnStartDate.ToString("d", new CultureInfo("en-GB")))
+            };
         }
     }
 }
