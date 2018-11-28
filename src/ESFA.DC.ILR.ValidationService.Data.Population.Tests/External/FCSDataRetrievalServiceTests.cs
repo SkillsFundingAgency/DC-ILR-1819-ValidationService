21e12f40a9dc3cabf89dc45389b68605af513a68
saqib2013 <5689652+saqib2013@users.noreply.github.com>
2018-11-28 13:42:00 +0000

    43472 - LearnAimRef_71 validation rule external cache model and data read logic added for FCS table EsfEligibilityRuleSectorSubjectAreaLevel.

--- src/ESFA.DC.ILR.ValidationService.Data.Population.Tests/External/FCSDataRetrievalServiceTests.cs
+++ src/ESFA.DC.ILR.ValidationService.Data.Population.Tests/External/FCSDataRetrievalServiceTests.cs
@@ -203,6 +203,466 @@ namespace ESFA.DC.ILR.ValidationService.Data.Population.Tests.External
             Assert.Equal(4, fcsa.Count);
         }
 
+        [Fact]
+        public async Task RetrieveEligibilityRuleSectorSubjectAreaLevelAsync()
+        {
+            var message = new TestMessage
+            {
+                LearningProviderEntity = new TestLearningProvider
+                {
+                    UKPRN = 1
+                },
+                Learners = new TestLearner[]
+                {
+                    new TestLearner()
+                    {
+                        LearningDeliveries = new TestLearningDelivery[]
+                        {
+                            new TestLearningDelivery
+                            {
+                                LearnAimRef = "123456",
+                                ConRefNumber = "ESF-123"
+                            },
+                            new TestLearningDelivery
+                            {
+                                LearnAimRef = "123457",
+                                ConRefNumber = "ESF-124"
+                            }
+                        }
+                    },
+                    new TestLearner()
+                    {
+                        LearningDeliveries = new TestLearningDelivery[]
+                        {
+                            new TestLearningDelivery
+                            {
+                                LearnAimRef = "123458",
+                                ConRefNumber = "ESF-125"
+                            },
+                            new TestLearningDelivery
+                            {
+                                LearnAimRef = "123459",
+                                ConRefNumber = null
+                            },
+                            new TestLearningDelivery
+                            {
+                                LearnAimRef = "123460"
+                            }
+                        }
+                    }
+                }
+            };
+
+            var allocations = new List<ContractAllocation>
+            {
+                new ContractAllocation
+                {
+                    ContractAllocationNumber = "ESF-123",
+                    FundingStreamCode = "Code1",
+                    FundingStreamPeriodCode = "PeriodCode1",
+                    Period = "R01",
+                    StartDate = new DateTime(2018, 8, 1),
+                    EndDate = new DateTime(2018, 8, 3),
+                    DeliveryUKPRN = 1,
+                    TenderSpecReference = "itt_29976",
+                    LotReference = "01"
+                },
+                new ContractAllocation
+                {
+                    ContractAllocationNumber = "ESF-124",
+                    FundingStreamCode = "Code1",
+                    FundingStreamPeriodCode = "PeriodCode1",
+                    Period = "R01",
+                    StartDate = new DateTime(2018, 8, 4),
+                    DeliveryUKPRN = 1,
+                    TenderSpecReference = "itt_29976",
+                    LotReference = "02"
+                },
+                new ContractAllocation
+                {
+                    ContractAllocationNumber = "ESF-125",
+                    FundingStreamCode = "Code1",
+                    FundingStreamPeriodCode = "PeriodCode1",
+                    Period = "R01",
+                    StartDate = new DateTime(2018, 8, 1),
+                    EndDate = new DateTime(2018, 8, 3),
+                    DeliveryUKPRN = 1,
+                    TenderSpecReference = "itt_29977",
+                    LotReference = "03"
+                },
+                new ContractAllocation
+                {
+                    ContractAllocationNumber = "ESF-123",
+                    FundingStreamCode = "Code1",
+                    FundingStreamPeriodCode = "PeriodCode1",
+                    Period = "R01",
+                    StartDate = new DateTime(2018, 8, 4),
+                    DeliveryUKPRN = 1,
+                    TenderSpecReference = "itt_29978",
+                    LotReference = "01"
+                },
+                new ContractAllocation
+                {
+                    ContractAllocationNumber = "ESF-123",
+                    FundingStreamCode = "Code1",
+                    FundingStreamPeriodCode = "PeriodCode1",
+                    Period = "R01",
+                    StartDate = new DateTime(2018, 8, 1),
+                    EndDate = new DateTime(2018, 8, 3),
+                    DeliveryUKPRN = 2,
+                    TenderSpecReference = "itt_29978",
+                    LotReference = "02"
+                },
+                new ContractAllocation
+                {
+                    ContractAllocationNumber = "ESF-125",
+                    FundingStreamCode = "Code1",
+                    FundingStreamPeriodCode = "PeriodCode1",
+                    Period = "R01",
+                    StartDate = new DateTime(2018, 8, 4),
+                    DeliveryUKPRN = 2
+                }
+            }.AsMockDbSet();
+
+            var esfSubjectAreaLevels = new List<EsfEligibilityRuleSectorSubjectAreaLevel>()
+            {
+                new EsfEligibilityRuleSectorSubjectAreaLevel()
+                {
+                    TenderSpecReference = "itt_29978",
+                    LotReference = "01",
+                    SectorSubjectAreaCode = 1.0M,
+                    MinLevelCode = "1",
+                    MaxLevelCode = "2"
+                },
+                new EsfEligibilityRuleSectorSubjectAreaLevel()
+                {
+                    TenderSpecReference = "itt_29978",
+                    LotReference = "02",
+                    SectorSubjectAreaCode = 5.2M,
+                    MinLevelCode = "1",
+                    MaxLevelCode = "2"
+                },
+                new EsfEligibilityRuleSectorSubjectAreaLevel()
+                {
+                    TenderSpecReference = "itt_29977",
+                    LotReference = "03",
+                    SectorSubjectAreaCode = 13.10M
+                },
+                new EsfEligibilityRuleSectorSubjectAreaLevel()
+                {
+                    TenderSpecReference = "itt_11115",
+                    LotReference = "17",
+                    SectorSubjectAreaCode = 1.0M,
+                    MinLevelCode = "1",
+                    MaxLevelCode = "2"
+                },
+                new EsfEligibilityRuleSectorSubjectAreaLevel()
+                {
+                    TenderSpecReference = "itt_11006",
+                    LotReference = "9",
+                    SectorSubjectAreaCode = 1.0M,
+                },
+            }.AsMockDbSet();
+
+            var fcsMock = new Mock<IFcsContext>();
+            fcsMock
+                .Setup(f => f.ContractAllocations)
+                .Returns(allocations);
+            fcsMock
+                .Setup(e => e.EsfEligibilityRuleSectorSubjectAreaLevel)
+                .Returns(esfSubjectAreaLevels);
+
+            var messageCacheMock = new Mock<ICache<IMessage>>();
+            messageCacheMock
+                .SetupGet(mc => mc.Item)
+                .Returns(message);
+
+            var fcsa = await this.NewService(fcsMock.Object, messageCacheMock.Object).RetrieveEligibilityRuleSectorSubjectAreaLevelAsync(CancellationToken.None);
+            fcsa.Should().HaveCount(2);
+        }
+
+        [Fact]
+        public async Task RetrieveEligibilityRuleSectorSubjectAreaLevelAsync_NoMessage()
+        {
+            var message = new TestMessage();
+            var allocations = new List<ContractAllocation>().AsMockDbSet();
+            var esfSubjectAreaLevels = new List<EsfEligibilityRuleSectorSubjectAreaLevel>().AsMockDbSet();
+
+            var fcsMock = new Mock<IFcsContext>();
+            fcsMock
+                .Setup(f => f.ContractAllocations)
+                .Returns(allocations);
+            fcsMock
+                .Setup(e => e.EsfEligibilityRuleSectorSubjectAreaLevel)
+                .Returns(esfSubjectAreaLevels);
+
+            var messageCacheMock = new Mock<ICache<IMessage>>();
+            messageCacheMock
+                .SetupGet(mc => mc.Item)
+                .Returns(message);
+
+            var fcsa = await this.NewService(fcsMock.Object, messageCacheMock.Object).RetrieveEligibilityRuleSectorSubjectAreaLevelAsync(CancellationToken.None);
+            fcsa.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task RetrieveEligibilityRuleSectorSubjectAreaLevelAsync_NoConRefNumbers()
+        {
+            var message = new TestMessage()
+            {
+                LearningProviderEntity = new TestLearningProvider
+                {
+                    UKPRN = 1
+                }
+            };
+            var allocations = new List<ContractAllocation>().AsMockDbSet();
+            var esfSubjectAreaLevels = new List<EsfEligibilityRuleSectorSubjectAreaLevel>().AsMockDbSet();
+
+            var fcsMock = new Mock<IFcsContext>();
+            fcsMock
+                .Setup(f => f.ContractAllocations)
+                .Returns(allocations);
+            fcsMock
+                .Setup(e => e.EsfEligibilityRuleSectorSubjectAreaLevel)
+                .Returns(esfSubjectAreaLevels);
+
+            var messageCacheMock = new Mock<ICache<IMessage>>();
+            messageCacheMock
+                .SetupGet(mc => mc.Item)
+                .Returns(message);
+
+            var fcsa = await this.NewService(fcsMock.Object, messageCacheMock.Object).RetrieveEligibilityRuleSectorSubjectAreaLevelAsync(CancellationToken.None);
+            fcsa.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task RetrieveEligibilityRuleSectorSubjectAreaLevelAsync_NoContractAllocations()
+        {
+            var message = new TestMessage()
+            {
+                LearningProviderEntity = new TestLearningProvider
+                {
+                    UKPRN = 1
+                },
+                Learners = new TestLearner[]
+                {
+                    new TestLearner()
+                    {
+                        LearningDeliveries = new TestLearningDelivery[]
+                        {
+                            new TestLearningDelivery
+                            {
+                                LearnAimRef = "123456",
+                                ConRefNumber = "ESF-123"
+                            }
+                        }
+                    }
+                }
+            };
+            var allocations = new List<ContractAllocation>().AsMockDbSet();
+            var esfSubjectAreaLevels = new List<EsfEligibilityRuleSectorSubjectAreaLevel>().AsMockDbSet();
+
+            var fcsMock = new Mock<IFcsContext>();
+            fcsMock
+                .Setup(f => f.ContractAllocations)
+                .Returns(allocations);
+            fcsMock
+                .Setup(e => e.EsfEligibilityRuleSectorSubjectAreaLevel)
+                .Returns(esfSubjectAreaLevels);
+
+            var messageCacheMock = new Mock<ICache<IMessage>>();
+            messageCacheMock
+                .SetupGet(mc => mc.Item)
+                .Returns(message);
+
+            var fcsa = await this.NewService(fcsMock.Object, messageCacheMock.Object).RetrieveEligibilityRuleSectorSubjectAreaLevelAsync(CancellationToken.None);
+            fcsa.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task RetrieveEligibilityRuleSectorSubjectAreaLevelAsync_NoSectorSubjectAreaLevelCodes()
+        {
+            var message = new TestMessage()
+            {
+                LearningProviderEntity = new TestLearningProvider
+                {
+                    UKPRN = 1
+                },
+                Learners = new TestLearner[]
+                {
+                    new TestLearner()
+                    {
+                        LearningDeliveries = new TestLearningDelivery[]
+                        {
+                            new TestLearningDelivery
+                            {
+                                LearnAimRef = "123456",
+                                ConRefNumber = "ESF-123"
+                            }
+                        }
+                    }
+                }
+            };
+
+            var allocations = new List<ContractAllocation>
+            {
+                new ContractAllocation
+                {
+                    ContractAllocationNumber = "ESF-123",
+                    FundingStreamCode = "Code1",
+                    FundingStreamPeriodCode = "PeriodCode1",
+                    Period = "R01",
+                    StartDate = new DateTime(2018, 8, 1),
+                    EndDate = new DateTime(2018, 8, 3),
+                    DeliveryUKPRN = 1,
+                    TenderSpecReference = "itt_29976",
+                    LotReference = "01"
+                }
+            }.AsMockDbSet();
+
+            var esfSubjectAreaLevels = new List<EsfEligibilityRuleSectorSubjectAreaLevel>().AsMockDbSet();
+
+            var fcsMock = new Mock<IFcsContext>();
+            fcsMock
+                .Setup(f => f.ContractAllocations)
+                .Returns(allocations);
+            fcsMock
+                .Setup(e => e.EsfEligibilityRuleSectorSubjectAreaLevel)
+                .Returns(esfSubjectAreaLevels);
+
+            var messageCacheMock = new Mock<ICache<IMessage>>();
+            messageCacheMock
+                .SetupGet(mc => mc.Item)
+                .Returns(message);
+
+            var fcsa = await this.NewService(fcsMock.Object, messageCacheMock.Object).RetrieveEligibilityRuleSectorSubjectAreaLevelAsync(CancellationToken.None);
+            fcsa.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ConRefNumbersFromMessage()
+        {
+            var message = new TestMessage()
+            {
+                LearningProviderEntity = new TestLearningProvider
+                {
+                    UKPRN = 1
+                },
+                Learners = new TestLearner[]
+                {
+                    new TestLearner()
+                    {
+                        LearningDeliveries = new TestLearningDelivery[]
+                        {
+                            new TestLearningDelivery
+                            {
+                                LearnAimRef = "123456",
+                                ConRefNumber = "ESF-123"
+                            }
+                        }
+                    }
+                }
+            };
+
+            var fcsMock = new Mock<IFcsContext>();
+            var messageCacheMock = new Mock<ICache<IMessage>>();
+            messageCacheMock
+                .SetupGet(mc => mc.Item)
+                .Returns(message);
+
+            var fcsa = this.NewService(fcsMock.Object, messageCacheMock.Object).ConRefNumbersFromMessage(message);
+            fcsa.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void ConRefNumbersFromMessage_NoMessage()
+        {
+            var fcsMock = new Mock<IFcsContext>();
+            var messageCacheMock = new Mock<ICache<IMessage>>();
+
+            var fcsa = this.NewService(fcsMock.Object, messageCacheMock.Object).ConRefNumbersFromMessage(null);
+            fcsa.Should().BeNull();
+        }
+
+        [Fact]
+        public void ConRefNumbersFromMessage_NoLearners()
+        {
+            var message = new TestMessage()
+            {
+                LearningProviderEntity = new TestLearningProvider
+                {
+                    UKPRN = 1
+                }
+            };
+            var fcsMock = new Mock<IFcsContext>();
+            var messageCacheMock = new Mock<ICache<IMessage>>();
+            messageCacheMock
+               .SetupGet(mc => mc.Item)
+               .Returns(message);
+
+            var fcsa = this.NewService(fcsMock.Object, messageCacheMock.Object).ConRefNumbersFromMessage(message);
+            fcsa.Should().BeNull();
+        }
+
+        [Fact]
+        public void ConRefNumbersFromMessage_NoLearnerDeliveries()
+        {
+            var message = new TestMessage()
+            {
+                LearningProviderEntity = new TestLearningProvider
+                {
+                    UKPRN = 1
+                },
+                Learners = new TestLearner[]
+                {
+                    new TestLearner()
+                    {
+                    }
+                }
+            };
+            var fcsMock = new Mock<IFcsContext>();
+            var messageCacheMock = new Mock<ICache<IMessage>>();
+            messageCacheMock
+               .SetupGet(mc => mc.Item)
+               .Returns(message);
+
+            var fcsa = this.NewService(fcsMock.Object, messageCacheMock.Object).ConRefNumbersFromMessage(message);
+            fcsa.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ConRefNumbersFromMessage_NoConRefNumbers()
+        {
+            var message = new TestMessage()
+            {
+                LearningProviderEntity = new TestLearningProvider
+                {
+                    UKPRN = 1
+                },
+                Learners = new TestLearner[]
+                {
+                    new TestLearner()
+                    {
+                        LearningDeliveries = new TestLearningDelivery[]
+                        {
+                            new TestLearningDelivery
+                            {
+                                LearnAimRef = "123456"
+                            }
+                        }
+                    }
+                }
+            };
+            var fcsMock = new Mock<IFcsContext>();
+            var messageCacheMock = new Mock<ICache<IMessage>>();
+            messageCacheMock
+               .SetupGet(mc => mc.Item)
+               .Returns(message);
+
+            var fcsa = this.NewService(fcsMock.Object, messageCacheMock.Object).ConRefNumbersFromMessage(message);
+            fcsa.Should().BeEmpty();
+        }
+
         private FCSDataRetrievalService NewService(IFcsContext fcs = null, ICache<IMessage> messageCache = null)
         {
             return new FCSDataRetrievalService(fcs, messageCache);
---
+++ src/ESFA.DC.ILR.ValidationService.Data/External/FCS/Interface/IEsfEligibilityRuleSectorSubjectAreaLevel.cs
@@ -0,0 +1,17 @@
+﻿namespace ESFA.DC.ILR.ValidationService.Data.External.FCS.Interface
+{
+    public interface IEsfEligibilityRuleSectorSubjectAreaLevel
+    {
+        int Id { get; set; }
+
+        string LotReference { get; set; }
+
+        string MaxLevelCode { get; set; }
+
+        string MinLevelCode { get; set; }
+
+        decimal? SectorSubjectAreaCode { get; set; }
+
+        string TenderSpecReference { get; set; }
+    }
+}
