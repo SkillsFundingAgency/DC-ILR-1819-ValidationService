23245f673dab59434236cbf7795fe560d12cfbfb
saqib2013 <5689652+saqib2013@users.noreply.github.com>
2018-12-12 15:24:12 +0000

    66077 - Validation error rules issue for null severity fixed by hangling the rule name case sensitivity issue in ValidationErrorsDataRetrievalService and ValidationErrorDataService and unit tests added as well for the new changes.

---
+++ src/ESFA.DC.ILR.ValidationService.Data.Population.Tests/External/ValidationErrorsDataRetrievalServiceTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.Data.ILR.ValidationErrors.Model;
+using ESFA.DC.Data.ILR.ValidationErrors.Model.Interfaces;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Data.Population.External;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ESFA.DC.ILR.ValidationService.Data.Population.Tests.External
+{
+    public class ValidationErrorsDataRetrievalServiceTests
+    {
+        [Fact]
+        public async Task RetrieveAsync()
+        {
+            var rules = new List<Rule>()
+            {
+                new Rule()
+                {
+                    Rulename = "RuleName1",
+                    Message = "Rule Name 1 Message",
+                    Severity = "E"
+                },
+                new Rule()
+                {
+                    Rulename = "RuleName2",
+                    Message = "Rule Name 2 Message",
+                    Severity = "W"
+                },
+                new Rule()
+                {
+                    Rulename = "RuleName3",
+                    Message = "Rule Name 3 Message",
+                    Severity = "F"
+                }
+            }.AsMockDbSet();
+
+            var validationErrorsMock = new Mock<IValidationErrors>();
+
+            validationErrorsMock.Setup(v => v.Rules).Returns(rules);
+
+            var validations = await this.NewService(validationErrors: validationErrorsMock.Object).RetrieveAsync(CancellationToken.None);
+            validations.Should().HaveCount(3);
+            validations.Keys.Should().Contain("rulename1"); // case insensitivity checks
+            validations.Keys.Should().Contain("RULENAME2"); // case insensitivity checks
+            validations.Keys.Should().Contain("ruleName3"); // case insensitivity checks
+        }
+
+        [Fact]
+        public async Task RetrieveAsync_EmptyCheck()
+        {
+            var rules = new List<Rule>();
+            var rulesDBContextMock = rules.AsMockDbSet();
+
+            var validationErrorsMock = new Mock<IValidationErrors>();
+
+            validationErrorsMock.Setup(v => v.Rules).Returns(rulesDBContextMock);
+
+            var validations = await this.NewService(validationErrors: validationErrorsMock.Object).RetrieveAsync(CancellationToken.None);
+
+            validations.Should().BeNullOrEmpty();
+        }
+
+            private ValidationErrorsDataRetrievalService NewService(IValidationErrors validationErrors = null)
+        {
+            return new ValidationErrorsDataRetrievalService(validationErrors: validationErrors);
+        }
+    }
+}
--- src/ESFA.DC.ILR.ValidationService.Data.Population/External/ValidationErrorsDataRetrievalService.cs
+++ src/ESFA.DC.ILR.ValidationService.Data.Population/External/ValidationErrorsDataRetrievalService.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿using ESFA.DC.Data.ILR.ValidationErrors.Model.Interfaces;
+using ESFA.DC.ILR.ValidationService.Data.Extensions;
+using ESFA.DC.ILR.ValidationService.Data.External.ValidationErrors.Model;
+using ESFA.DC.ILR.ValidationService.Data.Population.Interface;
+using ESFA.DC.ILR.ValidationService.Interface.Enum;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using ESFA.DC.Data.ILR.ValidationErrors.Model.Interfaces;
-using ESFA.DC.ILR.ValidationService.Data.External.ValidationErrors.Model;
-using ESFA.DC.ILR.ValidationService.Data.Population.Interface;
-using ESFA.DC.ILR.ValidationService.Interface.Enum;
 
 namespace ESFA.DC.ILR.ValidationService.Data.Population.External
 {
@@ -26,7 +27,7 @@ namespace ESFA.DC.ILR.ValidationService.Data.Population.External
         public async Task<IReadOnlyDictionary<string, ValidationError>> RetrieveAsync(CancellationToken cancellationToken)
         {
             return (await _validationErrors
-                .Rules
+                .Rules?
                 .ToListAsync(cancellationToken))
                 .ToDictionary(
                     ve => ve.Rulename,
@@ -35,7 +36,7 @@ namespace ESFA.DC.ILR.ValidationService.Data.Population.External
                         Message = ve.Message,
                         RuleName = ve.Rulename,
                         Severity = GetSeverity(ve.Severity)
-                    });
+                    }).ToCaseInsensitiveDictionary();
         }
 
         private Severity GetSeverity(string sev)
