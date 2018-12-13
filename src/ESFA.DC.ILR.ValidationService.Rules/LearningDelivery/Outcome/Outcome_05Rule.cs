3ba4d103c04d4ccac7f92553519e97695f0ece60
Cory-Wynn <CoryWynn1993@gmail.com>
2018-12-13 13:24:34 +0000

    44031 Outcome_05 Rule implementation

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/Outcome/Outcome_05Rule.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.Outcome
+{
+    public class Outcome_05Rule : AbstractRule, IRule<ILearner>
+    {
+        public Outcome_05Rule(IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler, RuleNameConstants.Outcome_05)
+        {
+        }
+
+        public void Validate(ILearner objectToValidate)
+        {
+            foreach (var learningDelivery in objectToValidate.LearningDeliveries)
+            {
+                if (ConditionMet(
+                    learningDelivery.OutcomeNullable,
+                    learningDelivery.LearnActEndDateNullable))
+                {
+                    HandleValidationError(
+                        objectToValidate.LearnRefNumber,
+                        learningDelivery.AimSeqNumber,
+                        BuildErrorMessageParameters(learningDelivery.OutcomeNullable));
+                }
+            }
+        }
+
+        public bool ConditionMet(int? outcome, DateTime? learnActEndDate)
+        {
+            return OutcomeConditionMet(outcome)
+                   && LearnActEndDateConditionMet(learnActEndDate);
+        }
+
+        public bool OutcomeConditionMet(int? outcome)
+        {
+            return outcome == 1;
+        }
+
+        public bool LearnActEndDateConditionMet(DateTime? learnActEndDate)
+        {
+            return !learnActEndDate.HasValue;
+        }
+
+        public IEnumerable<IErrorMessageParameter> BuildErrorMessageParameters(int? outcome)
+        {
+            return new[]
+            {
+                BuildErrorMessageParameter(PropertyNameConstants.Outcome, outcome)
+            };
+        }
+    }
+}
