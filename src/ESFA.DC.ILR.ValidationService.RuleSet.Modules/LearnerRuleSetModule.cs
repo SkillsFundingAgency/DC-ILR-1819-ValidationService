d88a34d8d6ebff53b4c3910cb48dfb393047ee07
jamie <jamie.coales@education.gov.uk>
2018-12-13 15:56:16 +0000

    #42473 Cross Entity Rule 47

--- src/ESFA.DC.ILR.ValidationService.RuleSet.Modules/LearnerRuleSetModule.cs
+++ src/ESFA.DC.ILR.ValidationService.RuleSet.Modules/LearnerRuleSetModule.cs
@@ -342,6 +342,7 @@ namespace ESFA.DC.ILR.ValidationService.RuleSet.Modules
                 typeof(QUALENT3_03Rule),
                 typeof(R07Rule),
                 typeof(R20Rule),
+                typeof(R47Rule),
                 typeof(R66Rule),
                 typeof(R68Rule),
                 typeof(R70Rule),
