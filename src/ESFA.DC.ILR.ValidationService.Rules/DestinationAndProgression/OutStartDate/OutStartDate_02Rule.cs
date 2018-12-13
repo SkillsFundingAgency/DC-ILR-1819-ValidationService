6ea5cda73eb03c9260d8624ae8638bbef8aae018
saqib2013 <5689652+saqib2013@users.noreply.github.com>
2018-12-13 16:54:27 +0000

    AB#42311 - OutStartDate_02 validation rule conditions implemented for DPOutcome data.

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/DestinationAndProgression/OutStartDate/OutStartDate_02Rule.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Data.Internal.AcademicYear.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.DestinationAndProgression.OutStartDate
+{
+    public class OutStartDate_02Rule : AbstractRule, IRule<ILearnerDestinationAndProgression>
+    {
+        private readonly IAcademicYearDataService _academicYearDataService;
+
+        public OutStartDate_02Rule(
+            IValidationErrorHandler validationErrorHandler,
+            IAcademicYearDataService academicYearDataService)
+            : base(validationErrorHandler, RuleNameConstants.OutStartDate_02)
+        {
+            _academicYearDataService = academicYearDataService;
+        }
+
+        public void Validate(ILearnerDestinationAndProgression objectToValidate)
+        {
+            if (objectToValidate?.DPOutcomes == null)
+            {
+                return;
+            }
+
+            DateTime oneYearAfterEndOfAcademicYear = _academicYearDataService.End().AddYears(1);
+
+            foreach (var dpOutCome in objectToValidate.DPOutcomes)
+            {
+                if (ConditionMet(dpOutCome.OutStartDate, oneYearAfterEndOfAcademicYear))
+                {
+                    HandleValidationError(
+                        learnRefNumber: objectToValidate.LearnRefNumber,
+                        errorMessageParameters: BuildErrorMessageParameters(dpOutCome.OutStartDate));
+                }
+            }
+        }
+
+        public bool ConditionMet(DateTime outStartDate, DateTime oneYearAfterEndOfAcademicYear) => outStartDate > oneYearAfterEndOfAcademicYear;
+
+        public IEnumerable<IErrorMessageParameter> BuildErrorMessageParameters(DateTime outStartDate)
+        {
+            return new[]
+            {
+                BuildErrorMessageParameter(PropertyNameConstants.OutStartDate, outStartDate)
+            };
+        }
+    }
+}
