363367b6792b3497249acdfb17151a985b9136bb
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-12-21 14:40:45 +0000

    AB#66664 - Basic scaffolding for Validation DP Actor

---
+++ src/ESFA.DC.ILR.ValidationService.ValidationDPActor.Interfaces/IValidationDPActor.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.ValidationService.ValidationDPActor.Interfaces.Models;
+using Microsoft.ServiceFabric.Actors;
+
+namespace ESFA.DC.ILR.ValidationService.ValidationDPActor.Interfaces
+{
+    /// <summary>
+    /// This interface defines the methods exposed by an actor.
+    /// Clients use this interface to interact with the actor that implements it.
+    /// </summary>
+    [ServiceContract]
+    public interface IValidationDPActor : IActor
+    {
+        [OperationContract]
+        Task<string> Validate(ValidationDPActorModel actorModel, CancellationToken cancellationToken);
+    }
+}
---
+++ src/ESFA.DC.ILR.ValidationService.ValidationDPActor/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using Autofac;
+using Autofac.Integration.ServiceFabric;
+using ESFA.DC.ILR.ValidationService.Modules;
+using ESFA.DC.ILR.ValidationService.Modules.Actor;
+using ESFA.DC.ILR.ValidationService.Stateless.Models;
+using ESFA.DC.ServiceFabric.Helpers;
+using Microsoft.ServiceFabric.Actors.Runtime;
+
+namespace ESFA.DC.ILR.ValidationService.ValidationDPActor
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
+        private static void Main()
+        {
+            try
+            {
+                var builder = BuildContainer();
+
+                // Register the Autofac magic for Service Fabric support.
+                builder.RegisterServiceFabricSupport();
+
+                // Register the actor service.
+                builder.RegisterActor<ValidationDPActor>(settings: new ActorServiceSettings
+                {
+                    ActorGarbageCollectionSettings = new ActorGarbageCollectionSettings(30, 30)
+                });
+
+                using (var container = builder.Build())
+                {
+                    Thread.Sleep(Timeout.Infinite);
+                }
+            }
+            catch (Exception e)
+            {
+                ActorEventSource.Current.ActorHostInitializationFailed(e.ToString());
+                throw;
+            }
+        }
+
+        private static ContainerBuilder BuildContainer()
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule<ActorValidationDPModule>();
+
+            // register logger
+            var configHelper = new ConfigurationHelper();
+            var loggerOptions =
+                configHelper.GetSectionValues<LoggerOptions>("LoggerSection");
+            containerBuilder.RegisterInstance(loggerOptions).As<LoggerOptions>().SingleInstance();
+            containerBuilder.RegisterModule<LoggerModule>();
+
+            return containerBuilder;
+        }
+    }
+}
