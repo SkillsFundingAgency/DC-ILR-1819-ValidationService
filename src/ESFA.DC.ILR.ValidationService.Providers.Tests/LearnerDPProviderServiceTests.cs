31fc75ca009ed8cf06c1ebaab2e6a0470a7304a9
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-12-21 15:44:09 +0000

    62424 - Learner DP provider Service implemented with tests

---
+++ src/ESFA.DC.ILR.ValidationService.Providers.Tests/LearnerDPProviderServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.Tests.Model;
+using ESFA.DC.ILR.ValidationService.Data.Interface;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ESFA.DC.ILR.ValidationService.Providers.Tests
+{
+    public class LearnerDPProviderServiceTests
+    {
+        [Fact]
+        public async Task Provide()
+        {
+            var learnerDPs = new List<TestLearnerDestinationAndProgression>();
+
+            var testMessage = new TestMessage()
+            {
+                LearnerDestinationAndProgressions = learnerDPs
+            };
+
+
+            var messageCacheMock = new Mock<ICache<IMessage>>();
+
+            messageCacheMock.SetupGet(mc => mc.Item).Returns(testMessage);
+
+            var learnerDPProviderService = new LearnerDPProviderService(messageCacheMock.Object);
+
+            (await learnerDPProviderService.ProvideAsync(CancellationToken.None)).Should().BeSameAs(learnerDPs);
+        }
+    }
+}
