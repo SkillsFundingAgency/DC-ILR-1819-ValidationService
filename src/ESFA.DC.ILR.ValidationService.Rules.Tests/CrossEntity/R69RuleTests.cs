641f59e348b92ef54602527be3b39d8de0dc1e8e
Salman Baig <salman.sfa@outlook.com>
2018-12-31 16:20:47 +0000

    Added R69

---
+++ src/ESFA.DC.ILR.ValidationService.Rules.Tests/CrossEntity/R69RuleTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.Tests.Model;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.CrossEntity;
+using ESFA.DC.ILR.ValidationService.Rules.Tests.Abstract;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Tests.CrossEntity
+{
+    public class R69RuleTests : AbstractRuleTests<R71Rule>
+    {
+        [Fact]
+        public void RuleName()
+        {
+            NewRule().RuleName.Should().Be("R69");
+        }
+
+        [Fact]
+        public void ValidationPasses()
+        {
+            var validationErrorHandlerMock = BuildValidationErrorHandlerMockForNoError();
+            var testMessage = new TestMessage()
+            {
+                LearnerDestinationAndProgressions = new List<ILearnerDestinationAndProgression>
+                {
+                    new TestLearnerDestinationAndProgression
+                    {
+                        LearnRefNumber = "12345",
+                        DPOutcomes = new IDPOutcome[]
+                        {
+                            new TestDPOutcome()
+                            {
+                                OutCode = 1,
+                                OutType = "A",
+                                OutStartDate = new DateTime(2018, 10, 10)
+                            },
+                            new TestDPOutcome()
+                            {
+                                OutCode = 2,
+                                OutType = "A",
+                                OutStartDate = new DateTime(2018, 10, 10)
+                            },
+                        }
+                    },
+                    new TestLearnerDestinationAndProgression
+                    {
+                        LearnRefNumber = "123456",
+                        DPOutcomes = new IDPOutcome[]
+                        {
+                            new TestDPOutcome()
+                            {
+                                OutCode = 1,
+                                OutType = "A",
+                                OutStartDate = new DateTime(2018, 10, 10)
+                            },
+                            new TestDPOutcome()
+                            {
+                                OutCode = 2,
+                                OutType = "A",
+                                OutStartDate = new DateTime(2018, 10, 10)
+                            },
+                        }
+                    }
+                }
+            };
+
+            NewRule(validationErrorHandlerMock.Object).Validate(testMessage);
+        }
+
+        [Fact]
+        public void ValidationFails()
+        {
+            var validationErrorHandlerMock = BuildValidationErrorHandlerMockForError();
+            var testMessage = new TestMessage()
+            {
+                LearnerDestinationAndProgressions = new List<ILearnerDestinationAndProgression>
+                {
+                    new TestLearnerDestinationAndProgression
+                    {
+                        LearnRefNumber = "12345",
+                        DPOutcomes = new IDPOutcome[]
+                        {
+                            new TestDPOutcome()
+                            {
+                                OutCode = 1,
+                                OutType = "A",
+                                OutStartDate = new DateTime(2018, 10, 10)
+                            },
+                            new TestDPOutcome()
+                            {
+                                OutCode = 1,
+                                OutType = "A",
+                                OutStartDate = new DateTime(2018, 10, 10)
+                            },
+                        }
+                    },
+                    new TestLearnerDestinationAndProgression
+                    {
+                        LearnRefNumber = "123456",
+                        DPOutcomes = new IDPOutcome[]
+                        {
+                            new TestDPOutcome()
+                            {
+                                OutCode = 1,
+                                OutType = "A",
+                                OutStartDate = new DateTime(2018, 10, 10)
+                            },
+                            new TestDPOutcome()
+                            {
+                                OutCode = 2,
+                                OutType = "A",
+                                OutStartDate = new DateTime(2018, 10, 10)
+                            },
+                        }
+                    }
+                }
+            };
+
+            NewRule(validationErrorHandlerMock.Object).Validate(testMessage);
+            validationErrorHandlerMock.Verify(h => h.Handle("R69", "12345", null, null));
+            validationErrorHandlerMock.Verify(h => h.Handle("R69", "123456", null, null), Times.Never);
+        }
+
+        private R69Rule NewRule(IValidationErrorHandler validationErrorHandler = null)
+        {
+            return new R69Rule(validationErrorHandler);
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.ValidationService.Rules/CrossEntity/R69Rule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Data.Extensions;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.CrossEntity
+{
+    public class R69Rule : AbstractRule, IRule<IMessage>
+    {
+        public R69Rule(IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler, RuleNameConstants.R69)
+        {
+        }
+
+        public void Validate(IMessage objectToValidate)
+        {
+            if (objectToValidate?.LearnerDestinationAndProgressions == null)
+            {
+                return;
+            }
+
+            var dpCoutcomes = objectToValidate.LearnerDestinationAndProgressions
+                .GroupBy(ldp => new
+                {
+                    ldp.LearnRefNumber,
+                    DpOutComeCount = ldp.DPOutcomes.GroupBy(x => new { x.OutCode, x.OutType, x.OutStartDate }).Select(y => y.Count())
+                })
+                .Where(x => x.Key.DpOutComeCount.Any(c => c > 1))
+                .Select(x => x.Key.LearnRefNumber);
+
+            foreach (var duplicate in dpCoutcomes)
+            {
+                HandleValidationError(duplicate);
+            }
+        }
+    }
+}
