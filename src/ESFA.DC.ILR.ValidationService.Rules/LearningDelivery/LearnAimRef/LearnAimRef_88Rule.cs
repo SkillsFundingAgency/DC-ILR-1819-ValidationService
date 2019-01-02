3df7511e93a39afa617a45d443e3b059e5e59e87
thales-man <msdn@thestripedlawn.co>
2019-01-02 16:22:59 +0000

    #66610 - learn aim ref rule 88 (withdrawal of funding condition fix)

--- src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/LearnAimRef/LearnAimRef_88Rule.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/LearnAimRef/LearnAimRef_88Rule.cs
@@ -3,7 +3,6 @@ using ESFA.DC.ILR.ValidationService.Data.External.LARS.Interface;
 using ESFA.DC.ILR.ValidationService.Interface;
 using ESFA.DC.ILR.ValidationService.Rules.Derived.Interface;
 using ESFA.DC.ILR.ValidationService.Utility;
-using System;
 using System.Linq;
 
 namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.LearnAimRef
@@ -38,14 +37,37 @@ namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.LearnAimRef
 
         /// <summary>
         /// Determines whether [has valid start range] [the specified validity].
+        /// caters for the custom and practice of setting the end date to before 
+        /// the start date as a means of withdrawing funding
         /// </summary>
         /// <param name="validity">The validity.</param>
         /// <param name="delivery">The delivery.</param>
         /// <returns>
         ///   <c>true</c> if [in valid start range] [the specified validity]; otherwise, <c>false</c>.
         /// </returns>
-        public bool HasValidStartRange(ILARSValidity validity, ILearningDelivery delivery) =>
-            It.IsBetween(delivery.LearnStartDate, validity.StartDate, validity.LastNewStartDate ?? validity.EndDate ?? DateTime.MaxValue);
+        public bool HasValidStartRange(ILARSValidity validity, ILearningDelivery delivery)
+        {
+            if (delivery.LearnStartDate < validity.StartDate)
+            {
+                return false;
+            }
+
+            if (validity.LastNewStartDate.HasValue && delivery.LearnStartDate > validity.LastNewStartDate)
+            {
+                return false;
+            }
+
+            if (validity.EndDate.HasValue && delivery.LearnStartDate > validity.EndDate)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        // this also works, but it does raise exceptions for a limited range of cases where the end date is less than the start date
+        // public bool HasValidStartRange(ILARSValidity validity, ILearningDelivery delivery) =>
+        //    SafeActions.Try(() => It.IsBetween(delivery.LearnStartDate, validity.StartDate, validity.LastNewStartDate ?? validity.EndDate ?? DateTime.MaxValue));
 
         /// <summary>
         /// Determines whether [has valid learning aim] [the specified delivery].
