fb6bf82dc2b438a2c4a1cfd0dd07de9f4cd201be
thales-man <msdn@thestripedlawn.co>
2019-01-03 13:59:09 +0000

    #44437 - didn't register rule as the work is halted. it's senseless building branches as it means you can't save work that doesn't build; and you may need too.

--- src/ESFA.DC.ILR.ValidationService.RuleSet.Modules/LearnerRuleSetModule.cs
+++ src/ESFA.DC.ILR.ValidationService.RuleSet.Modules/LearnerRuleSetModule.cs
@@ -413,6 +413,7 @@ namespace ESFA.DC.ILR.ValidationService.RuleSet.Modules
                 typeof(WorkPlaceEndDate_02Rule),
                 typeof(WorkPlaceStartDate_01Rule),
                 typeof(WorkPlaceStartDate_03Rule),
+                typeof(WorkPlaceEmpId_03Rule),
                 typeof(WorkPlaceEmpId_04Rule),
                 typeof(WorkPlaceEmpId_05Rule),
             };
