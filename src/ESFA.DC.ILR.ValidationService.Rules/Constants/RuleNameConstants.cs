be4f965a3761b3ba77bf9d2e25f579cd4f2e2206
jamie <jamie.coales@education.gov.uk>
2019-01-03 10:37:02 +0000

    LearnDelFamType07

--- src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
@@ -162,6 +162,7 @@
         public const string LearnDelFAMType_02 = "LearnDelFAMType_02";
         public const string LearnDelFAMType_03 = "LearnDelFAMType_03";
         public const string LearnDelFAMType_06 = "LearnDelFAMType_06";
+        public const string LearnDelFAMType_07 = "LearnDelFAMType_07";
         public const string LearnDelFAMType_08 = "LearnDelFAMType_08";
         public const string LearnDelFAMType_14 = "LearnDelFAMType_14";
         public const string LearnDelFAMType_18 = "LearnDelFAMType_18";
