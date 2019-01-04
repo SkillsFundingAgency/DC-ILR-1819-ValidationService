d66065c0c55bb2e0ab7d22ec9922640e41794555
Mike Childs <mike@mjcdev.co.uk>
2019-01-04 16:16:08 +0000

    derivedData_32 Rule
    Related Work Items: #42698, #57585

---
+++ src/ESFA.DC.ILR.ValidationService.Rules.Tests/Derived/DerivedData_32RuleTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Derived;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Tests.Derived
+{
+    public class DerivedData_32RuleTests
+    {
+        [Fact]
+        public void IsOpenApprenticeshipFundedProgramme_True()
+        {
+            var learningDeliveryMock = new Mock<ILearningDelivery>();
+
+            learningDeliveryMock.SetupGet(ld => ld.AimType).Returns(1);
+            learningDeliveryMock.SetupGet(ld => ld.FundModel).Returns(36);
+            learningDeliveryMock.SetupGet(ld => ld.LearnActEndDateNullable).Returns((DateTime?)null);
+
+            NewRule().IsOpenApprenticeshipFundedProgramme(learningDeliveryMock.Object).Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsOpenApprenticeshipFundedProgramme_False()
+        {
+            var learningDeliveryMock = new Mock<ILearningDelivery>();
+
+            learningDeliveryMock.SetupGet(ld => ld.AimType).Returns(0);
+
+            NewRule().IsOpenApprenticeshipFundedProgramme(learningDeliveryMock.Object).Should().BeFalse();
+        }
+
+        [Fact]
+        public void AimTypeConditionMet_True()
+        {
+            NewRule().AimTypeConditionMet(1).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(2)]
+        public void AimTypeConditionMet_False(int aimType)
+        {
+            NewRule().AimTypeConditionMet(aimType).Should().BeFalse();
+        }
+
+        [Fact]
+        public void FundModelConditionMet_True()
+        {
+            NewRule().FundModelConditionMet(36).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(35)]
+        [InlineData(37)]
+        public void FundModelConditionMet_False(int fundModel)
+        {
+            NewRule().FundModelConditionMet(fundModel).Should().BeFalse();
+        }
+
+        [Fact]
+        public void LearnStartDateConditionMet_True()
+        {
+            NewRule().LearnActEndDateConditionMet(null).Should().BeTrue();
+        }
+
+        [Fact]
+        public void LearnStartDateConditionMet_False()
+        {
+            NewRule().LearnActEndDateConditionMet(new DateTime(2018, 1, 1)).Should().BeFalse();
+        }
+
+        private DerivedData_32Rule NewRule()
+        {
+            return new DerivedData_32Rule();
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.ValidationService.Rules/Derived/DerivedData_32Rule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+using ESFA.DC.ILR.ValidationService.Rules.Derived.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Derived
+{
+    public class DerivedData_32Rule : IDerivedData_32Rule
+    {
+        public bool IsOpenApprenticeshipFundedProgramme(ILearningDelivery learningDelivery)
+        {
+            return AimTypeConditionMet(learningDelivery.AimType)
+                   && FundModelConditionMet(learningDelivery.FundModel)
+                   && LearnActEndDateConditionMet(learningDelivery.LearnActEndDateNullable);
+        }
+
+        public bool AimTypeConditionMet(int aimType) => aimType == TypeOfAim.ProgrammeAim;
+
+        public bool FundModelConditionMet(int fundModel) => fundModel == FundModelConstants.Apprenticeships;
+
+        public bool LearnActEndDateConditionMet(DateTime? learnActEndDate) => !learnActEndDate.HasValue;
+    }
+}
