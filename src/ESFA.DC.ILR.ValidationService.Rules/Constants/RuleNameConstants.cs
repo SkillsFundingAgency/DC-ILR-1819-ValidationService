007873d0147d2d963052eaabad27be673893703e
Saqib <5689652+saqib2013@users.noreply.github.com>
2019-01-05 18:02:08 +0000

    AB#42693 - R113 rule validation conditions added.

--- src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
@@ -318,6 +318,7 @@
         public const string R106 = "R106";
         public const string R108 = "R108";
         public const string R112 = "R112";
+        public const string R113 = "R113";
 
         public const string Schema = "Schema";
 
