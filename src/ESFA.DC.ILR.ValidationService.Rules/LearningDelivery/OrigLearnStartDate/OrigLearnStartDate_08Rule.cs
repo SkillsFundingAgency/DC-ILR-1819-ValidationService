65a7d9cd2ca65ed98b6fe46288347233d5f34eae
Salman Baig <salman.sfa@outlook.com>
2019-01-07 14:21:45 +0000

    orig learn start date 08

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/OrigLearnStartDate/OrigLearnStartDate_08Rule.cs
@@ -0,0 +1,79 @@
+﻿using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Data.External.LARS.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ESFA.DC.ILR.ValidationService.Rules.Query.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.OrigLearnStartDate
+{
+    public class OrigLearnStartDate_08Rule : AbstractRule, IRule<ILearner>
+    {
+        private const int FundModel99 = 99;
+
+        private readonly ILARSDataService _larsDataService;
+        private readonly ILearningDeliveryFAMQueryService _learningDeliveryFamQueryService;
+
+        public OrigLearnStartDate_08Rule(
+            ILARSDataService larsDataService,
+            ILearningDeliveryFAMQueryService learningDeliveryFamQueryService,
+            IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler, RuleNameConstants.OrigLearnStartDate_08)
+        {
+            _larsDataService = larsDataService;
+            _learningDeliveryFamQueryService = learningDeliveryFamQueryService;
+        }
+
+        public void Validate(ILearner objectToValidate)
+        {
+            foreach (var learningDelivery in objectToValidate.LearningDeliveries)
+            {
+                if (ConditionMet(
+                    learningDelivery.OrigLearnStartDateNullable,
+                    learningDelivery.FundModel,
+                    learningDelivery.LearningDeliveryFAMs,
+                    learningDelivery.LearnAimRef))
+                {
+                    HandleValidationError(
+                        objectToValidate.LearnRefNumber,
+                        learningDelivery.AimSeqNumber,
+                        BuildErrorMessageParameters(learningDelivery.OrigLearnStartDateNullable));
+                }
+            }
+        }
+
+        public bool ConditionMet(DateTime? origLearnStartDate, int fundModel, IReadOnlyCollection<ILearningDeliveryFAM> learningDeliveryFams, string learnAimRef)
+        {
+            return OrigLearnStartDateConditionMet(origLearnStartDate)
+                   && FundModelConditionMet(fundModel)
+                   && _learningDeliveryFamQueryService.HasLearningDeliveryFAMType(learningDeliveryFams, LearningDeliveryFAMTypeConstants.ADL)
+                   && LARSConditionMet(origLearnStartDate, learnAimRef);
+        }
+
+        public bool OrigLearnStartDateConditionMet(DateTime? origLearnStartDate)
+        {
+            return origLearnStartDate.HasValue;
+        }
+
+        public bool FundModelConditionMet(int fundModel)
+        {
+            return fundModel == FundModel99;
+        }
+
+        public bool LARSConditionMet(DateTime? origLearnStartDate, string learnAimRef)
+        {
+            return !_larsDataService.OrigLearnStartDateBetweenStartAndEndDateForValidityCategory(origLearnStartDate, learnAimRef, TypeOfLARSValidity.AdvancedLearnerLoan);
+        }
+
+        public IEnumerable<IErrorMessageParameter> BuildErrorMessageParameters(DateTime? origLearnStartDate)
+        {
+            return new[]
+            {
+                BuildErrorMessageParameter(PropertyNameConstants.OrigLearnStartDate, origLearnStartDate)
+            };
+        }
+    }
+}
