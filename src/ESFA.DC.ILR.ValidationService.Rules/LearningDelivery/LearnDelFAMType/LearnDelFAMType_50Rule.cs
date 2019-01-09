fd6e1299daebfdec98e9e67638238fcb9e34c2e8
jamie <jamie.coales@education.gov.uk>
2019-01-09 16:07:11 +0000

    LearnDelFamType50

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/LearnDelFAMType/LearnDelFAMType_50Rule.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Data.Extensions;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.LearnDelFAMType
+{
+    public class LearnDelFAMType_50Rule : AbstractRule, IRule<ILearner>
+    {
+        private readonly int[] _fundModels = { 25, 82, 10, 70, 99 };
+
+        public LearnDelFAMType_50Rule(
+            IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler, RuleNameConstants.LearnDelFAMType_50)
+        {
+        }
+
+        /// <summary>
+        /// Validates the specified object.
+        /// </summary>
+        /// <param name="learner">The object to validate.</param>
+        public void Validate(ILearner learner)
+        {
+            if (learner?.LearningDeliveries == null)
+            {
+                return;
+            }
+
+            foreach (var learningDelivery in learner.LearningDeliveries)
+            {
+                if (learningDelivery.LearningDeliveryFAMs == null || !_fundModels.Contains(learningDelivery.FundModel))
+                {
+                    continue;
+                }
+
+                foreach (var deliveryFam in learningDelivery.LearningDeliveryFAMs)
+                {
+                    if (deliveryFam.LearnDelFAMType.CaseInsensitiveEquals(LearningDeliveryFAMTypeConstants.LSF))
+                    {
+                        RaiseValidationMessage(learner.LearnRefNumber, learningDelivery, deliveryFam);
+                    }
+                }
+            }
+        }
+
+        private void RaiseValidationMessage(string learnRefNum, ILearningDelivery learningDelivery, ILearningDeliveryFAM thisMonitor)
+        {
+            var parameters = new List<IErrorMessageParameter>
+            {
+                BuildErrorMessageParameter(PropertyNameConstants.FundModel, learningDelivery.FundModel),
+                BuildErrorMessageParameter(PropertyNameConstants.LearnDelFAMType, thisMonitor.LearnDelFAMType)
+            };
+
+            HandleValidationError(learnRefNum, learningDelivery.AimSeqNumber, parameters);
+        }
+    }
+}
