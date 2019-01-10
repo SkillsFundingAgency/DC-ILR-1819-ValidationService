6c356bc33193576bcced66b3abd4461f84454d0d
jamie <jamie.coales@education.gov.uk>
2019-01-10 13:44:07 +0000

    added more codes to famcode constants

--- src/ESFA.DC.ILR.ValidationService.Rules/Constants/LearningDeliveryFAMCodeConstants.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/Constants/LearningDeliveryFAMCodeConstants.cs
@@ -6,54 +6,106 @@
         public const string SOF_ESFA_Adult = "105";
         public const string SOF_ESFA_1619 = "107";
         public const string SOF_LA = "108";
+        public const string SOF_Unassigned_110 = "110";
+        public const string SOF_Unassigned_111 = "111";
+        public const string SOF_Unassigned_112 = "112";
+        public const string SOF_Unassigned_113 = "113";
+        public const string SOF_Unassigned_114 = "114";
+        public const string SOF_Unassigned_115 = "115";
+        public const string SOF_Unassigned_116 = "116";
+        public const string SOF_Unassigned_117 = "117";
+        public const string SOF_Unassigned_118 = "118";
+        public const string SOF_Unassigned_119 = "119";
+        public const string SOF_Unassigned_120 = "120";
         public const string SOF_Other = "998";
-        public const string SOF_Unassigned = "110";
+
         public const string FFI_Fully = "1";
         public const string FFI_Co = "2";
+
         public const string EEF_Apprenticeship_19 = "2";
         public const string EEF_Apprenticeship_24 = "3";
         public const string EEF_Apprenticeship_Extended = "4";
+
         public const string RES_Code = "1";
+
         public const string LSF_Code = "1";
+
         public const string ADL_Code = "1";
+
         public const string ALB_Rate_1 = "1";
         public const string ALB_Rate_2 = "2";
         public const string ALB_Rate_3 = "3";
+
         public const string ASL_Personal = "1";
         public const string ASL_Neighbour = "2";
         public const string ASL_FamilyEnglishMathsLanguage = "3";
         public const string ASL_WiderFamily = "4";
+
         public const string ACT_ContractEmployer = "1";
         public const string ACT_ContractESFA = "2";
+
         public const string HHS_NoEmploymentWithChildren = "1";
         public const string HHS_NoEmploymentNoChildren = "2";
         public const string HHS_SingleWithChildren = "3";
-        public const string HHS_Withheld = "";
-        public const string HHS_NoneAbove = "";
+        public const string HHS_Withheld = "98";
+        public const string HHS_NoneAbove = "99";
+
         public const string LDM_OLASS = "034";
         public const string LDM_ProxyLearningAim = "118";
+        public const string LDM_GroupTrainingAssociation = "129";
+        public const string LDM_ApprenticeshipTrainingAgency = "130";
+        public const string LDM_MandationtoSkillsTraining = "318";
+        public const string LDM_RoTL = "328";
+        public const string LDM_PrincesTrustTeamProgramme = "331";
+        public const string LDM_SolentCity = "339";
+        public const string LDM_CommunityLearningMentalHealthPilot = "340";
         public const string LDM_Military = "346";
         public const string LDM_SteelRedundancy = "347";
-        public const string LDM_SolentCity = "339";
+        public const string LDM_HESA_GeneratedILRfile = "352";
         public const string LDM_NonApprenticeshipSportingExcellence = "353";
         public const string LDM_NonApprenticeshipTheatre = "354";
         public const string LDM_NonApprenticeshipSeaFishing = "355";
         public const string LDM_OwnEmployeeAprenticeship = "356";
-        public const string LDM_GroupTrainingAssociation = "129";
-        public const string LDM_ApprenticeshipTrainingAgency = "130";
-        public const string LDM_PrincesTrustTeamProgramme = "331";
-        public const string LDM_HESA_GeneratedILRfile = "352";
-        public const string LDM_CommunityLearningMentalHealthPilot = "340";
-        public const string LDM_RoTL = "328";
-        public const string LDM_MandationtoSkillsTraining = "318";
-        public const string LDM_Pilot = "358";
-        public const string LDM_LowWages = "363";
         public const string LDM_ProcuredAdultEducationBudget = "357";
+        public const string LDM_Pilot = "358";
         public const string LDM_CareerLearningPilot = "359";
+        public const string LDM_LowWages = "363";
+
+        public const string NSA_FashionRetail = "1";
+        public const string NSA_Manufacturing = "2";
+        public const string NSA_FinancialServices = "3";
+        public const string NSA_Construction = "4";
+        public const string NSA_FoodAndDrinkManufacturing = "5";
+        public const string NSA_Nuclear = "6";
+        public const string NSA_ProcessIndustries = "7";
+        public const string NSA_CreativeAndCultural = "8";
+        public const string NSA_Hospitality = "9";
+        public const string NSA_SportAndActiveLeisure = "10";
+        public const string NSA_Retail = "11";
+        public const string NSA_MaterialsProductionAndSupply = "12";
+        public const string NSA_NationalEnterpriseAcademy = "13";
         public const string NSA_Socialcare = "14";
+        public const string NSA_InformationTechnology = "15";
+        public const string NSA_Power = "16";
+        public const string NSA_RailEngineering = "17";
+        public const string NSA_EnvironmentalTechnologies = "18";
+        public const string NSA_Logistics = "19";
+        public const string NSA_Health = "20";
+
         public const string FLN_FEML = "1";
-        public const string POD_FItyPercent = "5";
+
+        public const string POD_0Percent = "1";
+        public const string POD_1To9Percent = "2";
+        public const string POD_10To24Percent = "3";
+        public const string POD_25To49Percent = "4";
+        public const string POD_50To74Percent = "5";
+        public const string POD_75To99Percent = "6";
+        public const string POD_100Percent = "7";
+
+        public const string HEM_Funded = "1";
         public const string HEM_Award = "3";
+        public const string HEM_Exception = "5";
+
         public const string WPP_DWP = "1";
     }
 }
