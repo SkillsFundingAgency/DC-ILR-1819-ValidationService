cde13a087fc30c104a72460ace9d6248438f992d
Gayatri <gayatrippatel@hotmail.com>
2019-01-14 14:41:11 +0000

    44562-refactoring

--- src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/ProvSpecDelMonOccur/ProvSpecDelMonOccur_01Rule.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/ProvSpecDelMonOccur/ProvSpecDelMonOccur_01Rule.cs
@@ -1,18 +1,18 @@
-﻿using ESFA.DC.ILR.Model.Interface;
-using ESFA.DC.ILR.ValidationService.Interface;
-using ESFA.DC.ILR.ValidationService.Rules.Abstract;
-using ESFA.DC.ILR.ValidationService.Rules.Constants;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using ESFA.DC.ILR.ValidationService.Rules.Query.Interface;
-
-namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.ProvSpecDelMonOccur
+﻿namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.ProvSpecDelMonOccur
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using ESFA.DC.ILR.Model.Interface;
+    using ESFA.DC.ILR.ValidationService.Data.Extensions;
+    using ESFA.DC.ILR.ValidationService.Interface;
+    using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+    using ESFA.DC.ILR.ValidationService.Rules.Constants;
+    using ESFA.DC.ILR.ValidationService.Rules.Query.Interface;
+
     public class ProvSpecDelMonOccur_01Rule : AbstractRule, IRule<ILearner>
     {
-        private readonly HashSet<string> _validProvSpecDelMonOccurValues = new HashSet<string>() { "A", "B", "C", "D" };
-
         public ProvSpecDelMonOccur_01Rule(IValidationErrorHandler validationErrorHandler)
             : base(validationErrorHandler, RuleNameConstants.ProvSpecDelMonOccur_01Rule)
         {
@@ -27,7 +27,11 @@ namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.ProvSpecDelMonOcc
 
             foreach (var learningDelivery in objectToValidate.LearningDeliveries)
             {
-                if (learningDelivery.ProviderSpecDeliveryMonitorings == null) continue;
+                if (learningDelivery.ProviderSpecDeliveryMonitorings == null)
+                {
+                    continue;
+                }
+
                 foreach (var providerSpecDeliveryMonitoring in learningDelivery.ProviderSpecDeliveryMonitorings)
                 {
                     if (ConditionMet(providerSpecDeliveryMonitoring.ProvSpecDelMonOccur))
@@ -43,7 +47,7 @@ namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.ProvSpecDelMonOcc
 
         public bool ConditionMet(string provSpecDelMonOccur)
         {
-            return !string.IsNullOrWhiteSpace(provSpecDelMonOccur) && !Monitoring.Delivery.ValidProvSpecDelMonOccurValues.Contains(provSpecDelMonOccur);
+            return !string.IsNullOrWhiteSpace(provSpecDelMonOccur) && !Monitoring.Delivery.ValidProvSpecDelMonOccurValues.Any(x => x.CaseInsensitiveEquals(provSpecDelMonOccur));
         }
 
         public IEnumerable<IErrorMessageParameter> BuildErrorMessageParameters(string provSpecDelMonOccur)
