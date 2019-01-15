1af7bd27a4e0749b5dd5843221d3018dbe6627e0
Salman Baig <salman.sfa@outlook.com>
2019-01-15 17:05:14 +0000

    in progress...

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/Derived/DerivedData_12Rule.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Data.Extensions;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+using ESFA.DC.ILR.ValidationService.Rules.Derived.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Query.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Derived
+{
+    public class DerivedData_12Rule : IDerivedData_12Rule //: IDerivedData_32Rule
+    {
+        private readonly HashSet<int> ValidEmploymentStatusCodes = new HashSet<int>() { 1, 2 };
+        private const int ValidEmploymentStatusCodeForLDM = 4;
+
+        private readonly ILearnerEmploymentStatusMonitoringQueryService _learnerEmploymentStatusMonitoringQueryService;
+        private readonly ILearningDeliveryFAMQueryService _learningDeliveryFamQueryService;
+
+        public DerivedData_12Rule(
+            ILearnerEmploymentStatusMonitoringQueryService learnerEmploymentStatusMonitoringQueryService,
+            ILearningDeliveryFAMQueryService learningDeliveryFamQueryService)
+        {
+            _learnerEmploymentStatusMonitoringQueryService = learnerEmploymentStatusMonitoringQueryService;
+            _learningDeliveryFamQueryService = learningDeliveryFamQueryService;
+        }
+
+        public bool IsAdultSkillsFundedOnBenefits(
+            IReadOnlyCollection<ILearnerEmploymentStatus> employmentStatusMonitorings,
+            ILearningDelivery learningDelivery)
+        {
+            return FundModelConditionMet(learningDelivery.FundModel)
+                   && EmploymentStatusMonitoringConditionMet(employmentStatusMonitorings, learningDelivery);
+        }
+
+        public bool FundModelConditionMet(int fundModel)
+        {
+            return fundModel == TypeOfFunding.AdultSkills;
+        }
+
+        public bool EmploymentStatusMonitoringConditionMet(IReadOnlyCollection<ILearnerEmploymentStatus> learnerEmploymentStatuses, ILearningDelivery learningDelivery)
+        {
+            var latest = learnerEmploymentStatuses
+                .Where(x => x.DateEmpStatApp <= learningDelivery.LearnStartDate)
+                .OrderByDescending(x => x.DateEmpStatApp)
+                .FirstOrDefault();
+
+            if (_learnerEmploymentStatusMonitoringQueryService
+                .HasAnyEmploymentStatusMonitoringTypeAndCodesForEmploymentStatus(
+                    latest,
+                    Monitoring.EmploymentStatus.Types.BenefitStatusIndicator,
+                    ValidEmploymentStatusCodes))
+            {
+                return true;
+            }
+
+            if (FamConditionMet(learningDelivery.LearningDeliveryFAMs) &&
+                 _learnerEmploymentStatusMonitoringQueryService
+                .HasAnyEmploymentStatusMonitoringTypeAndCodeForEmploymentStatus(
+                    latest,
+                    Monitoring.EmploymentStatus.Types.BenefitStatusIndicator,
+                    ValidEmploymentStatusCodeForLDM))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool FamConditionMet(IReadOnlyCollection<ILearningDeliveryFAM> learningDeliveryFams)
+        {
+            return _learningDeliveryFamQueryService.HasLearningDeliveryFAMCodeForType(
+                learningDeliveryFams,
+                LearningDeliveryFAMTypeConstants.LDM,
+                LearningDeliveryFAMCodeConstants.LDM_MandationtoSkillsTraining);
+        }
+    }
+}
--- src/ESFA.DC.ILR.ValidationService.Rules/Query/Interface/ILearnerEmploymentStatusMonitoringQueryService.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/Query/Interface/ILearnerEmploymentStatusMonitoringQueryService.cs
@@ -10,5 +10,10 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Query.Interface
         bool HasAnyEmploymentStatusMonitoringTypeMoreThanOnce(IEnumerable<IEmploymentStatusMonitoring> employmentStatusMonitorings, IEnumerable<string> esmTypes);
 
         IEnumerable<string> GetDuplicatedEmploymentStatusMonitoringTypesForTypes(IEnumerable<IEmploymentStatusMonitoring> employmentStatusMonitorings, IEnumerable<string> esmTypes);
+
+        bool HasAnyEmploymentStatusMonitoringTypeAndCodesForEmploymentStatus(ILearnerEmploymentStatus learnerEmploymentStatus, string esmType, IEnumerable<int> esmCodes);
+
+        bool HasAnyEmploymentStatusMonitoringTypeAndCodeForEmploymentStatus(ILearnerEmploymentStatus learnerEmploymentStatus, string esmType, int esmCode);
+
     }
 }
