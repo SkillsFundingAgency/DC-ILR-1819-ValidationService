0a29da811f1ff522ee0ea8539d09394dc0abd757
Mike Childs <mike@mjcdev.co.uk>
2019-01-15 14:43:56 +0000

    Moving Extensions Project under the Data Project, Writing some Tests to cover existing scenarios prior to refactor

---
+++ src/ESFA.DC.ILR.ValidationService.Data.Population/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ESFA.DC.ILR.ValidationService.Data.Extensions
+{
+    public static class IQueryableExtensions
+    {
+        public static Task<Dictionary<string, TValue>> ToCaseInsensitiveAsyncDictionary<TValue, TInput>(this IQueryable<TInput> enumerable, Func<TInput, string> keySelector, Func<TInput, TValue> valueSelector, CancellationToken cancellationToken)
+        {
+            return enumerable.ToDictionaryAsync(keySelector, valueSelector, StringComparer.OrdinalIgnoreCase, cancellationToken);
+        }
+    }
+}
