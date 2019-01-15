29ee5d5f7d9012eb502ffdc316899b145013e584
Ashley Beecham <ash@momentoussolutions.co.uk>
2019-01-15 11:38:37 +0000

    57545 - Org Package updated with refactorings. SF integegration to be verified

--- src/ESFA.DC.ILR.ValidationService.Data.Population/External/OrganisationsDataRetrievalService.cs
+++ src/ESFA.DC.ILR.ValidationService.Data.Population/External/OrganisationsDataRetrievalService.cs
@@ -3,24 +3,24 @@ using System.Data.Entity;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using ESFA.DC.Data.Organisatons.Model.Interface;
 using ESFA.DC.ILR.Model.Interface;
 using ESFA.DC.ILR.ValidationService.Data.External.Organisation.Model;
 using ESFA.DC.ILR.ValidationService.Data.Interface;
 using ESFA.DC.ILR.ValidationService.Data.Population.Interface;
+using ESFA.DC.ReferenceData.Organisations.Model.Interface;
 
 namespace ESFA.DC.ILR.ValidationService.Data.Population.External
 {
     public class OrganisationsDataRetrievalService : AbstractOrganisationsDataRetrievalService, IOrganisationsDataRetrievalService
     {
-        private readonly IOrganisations _organisations;
+        private readonly IOrganisationsContext _organisations;
 
         public OrganisationsDataRetrievalService()
               : base(null)
         {
         }
 
-        public OrganisationsDataRetrievalService(IOrganisations organisations, ICache<IMessage> messageCache)
+        public OrganisationsDataRetrievalService(IOrganisationsContext organisations, ICache<IMessage> messageCache)
               : base(messageCache)
         {
             _organisations = organisations;
@@ -32,14 +32,14 @@ namespace ESFA.DC.ILR.ValidationService.Data.Population.External
 
             return await _organisations
                 .MasterOrganisations
-                .Where(o => ukprns.Contains(o.UKPRN))
+                .Where(o => ukprns.Contains(o.Ukprn))
                 .ToDictionaryAsync(
-                    o => o.UKPRN,
+                    o => o.Ukprn,
                     o => new Organisation
                     {
-                        UKPRN = o.Org_Details?.UKPRN,
-                        LegalOrgType = o.Org_Details?.LegalOrgType,
-                        PartnerUKPRN = o.Org_PartnerUKPRN.Any(op => op.UKPRN == o.UKPRN)
+                        UKPRN = o.OrgDetail?.Ukprn,
+                        LegalOrgType = o.OrgDetail?.LegalOrgType,
+                        PartnerUKPRN = o.OrgPartnerUkprns.Any(op => op.Ukprn == o.Ukprn)
                     }, cancellationToken);
         }
     }
