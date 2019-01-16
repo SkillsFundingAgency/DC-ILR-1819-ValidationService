e9a1d7ac60443ad7aa0278d2642689382b809feb
jamie <jamie.coales@education.gov.uk>
2019-01-16 13:28:53 +0000

    DateOfBirth43

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/Learner/DateOfBirth/DateOfBirth_43Rule.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+using ESFA.DC.ILR.ValidationService.Rules.Derived.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Learner.DateOfBirth
+{
+    public class DateOfBirth_43Rule : AbstractRule, IRule<ILearner>
+    {
+        private const int MinAge = 15;
+
+        private readonly IDerivedData_23Rule _derivedData23;
+
+        public DateOfBirth_43Rule(
+            IDerivedData_23Rule derivedData23,
+            IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler, RuleNameConstants.DateOfBirth_43)
+        {
+            _derivedData23 = derivedData23;
+        }
+
+        public void Validate(ILearner learner)
+        {
+            var learningDelivery =
+                learner?.LearningDeliveries?.FirstOrDefault(ld => ld.FundModel == TypeOfFunding.EuropeanSocialFund);
+
+            if (learningDelivery == null)
+            {
+                return;
+            }
+
+            if (_derivedData23.GetLearnersAgeAtStartOfESFContract(learner, learner.LearningDeliveries) < MinAge)
+            {
+                RaiseValidationMessage(learner, learningDelivery);
+            }
+        }
+
+        private void RaiseValidationMessage(ILearner learner, ILearningDelivery learningDelivery)
+        {
+            var parameters = new List<IErrorMessageParameter>
+            {
+                BuildErrorMessageParameter(PropertyNameConstants.FundModel, TypeOfFunding.EuropeanSocialFund),
+                BuildErrorMessageParameter(PropertyNameConstants.DateOfBirth, learner.DateOfBirthNullable)
+            };
+
+            HandleValidationError(learner.LearnRefNumber, learningDelivery.AimSeqNumber, parameters);
+        }
+    }
+}
