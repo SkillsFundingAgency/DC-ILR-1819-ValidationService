722e80254788e48bdbbe8a46dcd39c37e8976c34
Ashley Beecham <ash@momentoussolutions.co.uk>
2019-01-18 15:44:23 +0000

    42392 - DD19 implemented

---
+++ src/ESFA.DC.ILR.ValidationService.Rules.Tests/Derived/DerivedData_19RuleTests.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using ESFA.DC.ILR.Tests.Model;
+using ESFA.DC.ILR.ValidationService.Rules.Derived;
+using FluentAssertions;
+using Xunit;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Tests.Derived
+{
+    public class DerivedData_19RuleTests
+    {
+        [Fact]
+        public void LearningDeliveriesForProgrammeAim_ReturnsNull_NoDeliveries()
+        {
+            NewRule().LearningDeliveriesForProgrammeAim(null).Should().BeNullOrEmpty();
+        }
+
+        [Fact]
+        public void LearningDeliveriesForProgrammeAim_ReturnsNull_MisMatch()
+        {
+            var learningDeliveries = new List<TestLearningDelivery>
+            {
+                new TestLearningDelivery()
+                {
+                    ProgTypeNullable = 25,
+                    StdCodeNullable = 1,
+                    AimType = 2,
+                    LearnPlanEndDate = new DateTime(2015, 1, 1)
+                }
+            };
+
+            NewRule().LearningDeliveriesForProgrammeAim(learningDeliveries).Should().BeNullOrEmpty();
+        }
+
+        [Fact]
+        public void LearningDeliveriesForProgrammeAim_Returns()
+        {
+            var learningDeliveryOne = new TestLearningDelivery()
+            {
+                ProgTypeNullable = 25,
+                StdCodeNullable = 1,
+                AimType = 1,
+                LearnPlanEndDate = new DateTime(2015, 1, 1)
+            };
+
+            var learningDeliveryTwo = new TestLearningDelivery()
+            {
+                ProgTypeNullable = 25,
+                StdCodeNullable = 1,
+                AimType = 2,
+                LearnPlanEndDate = new DateTime(2015, 1, 1)
+            };
+
+            var learningDeliveries = new List<TestLearningDelivery>
+            {
+                learningDeliveryOne,
+                learningDeliveryTwo
+            };
+
+            NewRule().LearningDeliveriesForProgrammeAim(learningDeliveries).Should().BeEquivalentTo(learningDeliveryOne);
+        }
+
+        [Fact]
+        public void LearningDeliveriesForProgrammeAim_ReturnsMultiple()
+        {
+            var learningDeliveries = new List<TestLearningDelivery>
+            {
+                new TestLearningDelivery()
+                {
+                    ProgTypeNullable = 25,
+                    StdCodeNullable = 1,
+                    AimType = 1,
+                    LearnPlanEndDate = new DateTime(2015, 1, 1)
+                },
+                new TestLearningDelivery()
+                {
+                    ProgTypeNullable = 26,
+                    StdCodeNullable = 1,
+                    AimType = 1,
+                    LearnPlanEndDate = new DateTime(2015, 1, 1)
+                }
+            };
+
+            NewRule().LearningDeliveriesForProgrammeAim(learningDeliveries).Should().BeEquivalentTo(learningDeliveries);
+        }
+
+        [Fact]
+        public void LearningDeliveryHasApprenticeshipStandardType_False_Null()
+        {
+            NewRule().LearningDeliveryHasApprenticeshipStandardType(null).Should().BeFalse();
+        }
+
+        [Fact]
+        public void LearningDeliveryHasApprenticeshipStandardType_False()
+        {
+            var learningelivery = new TestLearningDelivery()
+            {
+                ProgTypeNullable = 26,
+                StdCodeNullable = 1,
+                AimType = 1,
+                LearnPlanEndDate = new DateTime(2015, 1, 1)
+            };
+
+            NewRule().LearningDeliveryHasApprenticeshipStandardType(learningelivery).Should().BeFalse();
+        }
+
+        [Fact]
+        public void LearningDeliveryHasApprenticeshipStandardType_True()
+        {
+            var learningelivery = new TestLearningDelivery()
+            {
+                ProgTypeNullable = 25,
+                StdCodeNullable = 1,
+                AimType = 1,
+                LearnPlanEndDate = new DateTime(2015, 1, 1)
+            };
+
+            NewRule().LearningDeliveryHasApprenticeshipStandardType(learningelivery).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Derive_Returns_Date()
+        {
+            var earliestLearningDelivery = new TestLearningDelivery()
+            {
+                ProgTypeNullable = 25,
+                StdCodeNullable = 1,
+                AimType = 1,
+                LearnPlanEndDate = new DateTime(2015, 1, 1)
+            };
+
+            var latestLearningDelivery = new TestLearningDelivery()
+            {
+                ProgTypeNullable = 25,
+                StdCodeNullable = 1,
+                AimType = 1,
+                LearnPlanEndDate = new DateTime(2017, 1, 1)
+            };
+
+            var learner = new TestLearner()
+            {
+                LearningDeliveries = new TestLearningDelivery[]
+                {
+                    latestLearningDelivery,
+                    earliestLearningDelivery
+                }
+            };
+
+            NewRule().Derive(learner.LearningDeliveries, latestLearningDelivery).Should().Be(new DateTime(2017, 1, 1));
+        }
+
+        [Fact]
+        public void Derive_Returns_Null_NoDeliveries()
+        {
+            NewRule().Derive(null, null).Should().BeNull();
+        }
+
+        [Fact]
+        public void Derive_Returns_Null_SingleDelivery()
+        {
+            var earliestLearningDelivery = new TestLearningDelivery()
+            {
+                ProgTypeNullable = 25,
+                StdCodeNullable = 1,
+                AimType = 2,
+                LearnPlanEndDate = new DateTime(2015, 1, 1)
+            };
+
+            var latestLearningDelivery = new TestLearningDelivery()
+            {
+                ProgTypeNullable = 25,
+                StdCodeNullable = 1,
+                AimType = 2,
+                LearnPlanEndDate = new DateTime(2017, 1, 1)
+            };
+
+            var learner = new TestLearner()
+            {
+                LearningDeliveries = new TestLearningDelivery[]
+                {
+                    latestLearningDelivery,
+                    earliestLearningDelivery
+                }
+            };
+
+            NewRule().Derive(null, latestLearningDelivery).Should().BeNull();
+        }
+
+        [Fact]
+        public void Derive_Returns_Null_ProgType()
+        {
+            var earliestLearningDelivery = new TestLearningDelivery()
+            {
+                ProgTypeNullable = 26,
+                StdCodeNullable = 1,
+                AimType = 1,
+                LearnPlanEndDate = new DateTime(2015, 1, 1)
+            };
+
+            var latestLearningDelivery = new TestLearningDelivery()
+            {
+                ProgTypeNullable = 26,
+                StdCodeNullable = 1,
+                AimType = 1,
+                LearnPlanEndDate = new DateTime(2017, 1, 1)
+            };
+
+            var learner = new TestLearner()
+            {
+                LearningDeliveries = new TestLearningDelivery[]
+                {
+                    latestLearningDelivery,
+                    earliestLearningDelivery
+                }
+            };
+
+            NewRule().Derive(learner.LearningDeliveries, latestLearningDelivery).Should().BeNull();
+        }
+
+        [Fact]
+        public void Derive_Returns_Null_NoProgAims()
+        {
+            var earliestLearningDelivery = new TestLearningDelivery()
+            {
+                ProgTypeNullable = 25,
+                StdCodeNullable = 1,
+                AimType = 2,
+                LearnPlanEndDate = new DateTime(2015, 1, 1)
+            };
+
+            var latestLearningDelivery = new TestLearningDelivery()
+            {
+                ProgTypeNullable = 25,
+                StdCodeNullable = 1,
+                AimType = 2,
+                LearnPlanEndDate = new DateTime(2017, 1, 1)
+            };
+
+            var learner = new TestLearner()
+            {
+                LearningDeliveries = new TestLearningDelivery[]
+                {
+                    latestLearningDelivery,
+                    earliestLearningDelivery
+                }
+            };
+
+            NewRule().Derive(learner.LearningDeliveries, latestLearningDelivery).Should().BeNull();
+        }
+
+        private DerivedData_19Rule NewRule()
+        {
+            return new DerivedData_19Rule();
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.ValidationService.Rules/Derived/DerivedData_19Rule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+using ESFA.DC.ILR.ValidationService.Rules.Derived.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Derived
+{
+    public class DerivedData_19Rule : IDerivedData_19Rule
+    {
+        public DateTime? Derive(IEnumerable<ILearningDelivery> learningDeliveries, ILearningDelivery learningDelivery)
+        {
+            var programmeAims = LearningDeliveriesForProgrammeAim(learningDeliveries);
+
+            if (LearningDeliveryHasApprenticeshipStandardType(learningDelivery) && programmeAims.Any())
+            {
+                return LatestLearningDeliveryLearnPlanEndDateFor(programmeAims, learningDelivery.ProgTypeNullable, learningDelivery.StdCodeNullable);
+            }
+
+            return null;
+        }
+
+        public DateTime? LatestLearningDeliveryLearnPlanEndDateFor(IEnumerable<ILearningDelivery> learningDeliveries, long? progType, long? stdCode)
+        {
+            return learningDeliveries
+                    .Where(ld =>
+                    ld.ProgTypeNullable == progType
+                    && ld.StdCodeNullable == stdCode)
+                .Max(ld => (DateTime?)ld.LearnPlanEndDate);
+        }
+
+        public bool LearningDeliveryHasApprenticeshipStandardType(ILearningDelivery learningDelivery)
+        {
+            return learningDelivery != null
+                ? learningDelivery.ProgTypeNullable == TypeOfLearningProgramme.ApprenticeshipStandard
+                : false;
+        }
+
+        public IEnumerable<ILearningDelivery> LearningDeliveriesForProgrammeAim(IEnumerable<ILearningDelivery> learningDeliveries)
+        {
+            return learningDeliveries?
+                .Where(ld =>
+                ld.AimType == TypeOfAim.ProgrammeAim).ToList() ?? new List<ILearningDelivery>();
+        }
+    }
+}
