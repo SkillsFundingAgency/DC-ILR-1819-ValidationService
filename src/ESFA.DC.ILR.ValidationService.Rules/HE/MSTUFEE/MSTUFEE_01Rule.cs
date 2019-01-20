2d4aa69baee9ad9cbd640a6ede7c56673a929dc1
Anil Pabbati <anil.pabbati@outlook.com>
2019-01-20 16:24:40 +0000

    43099-RB-Val-MSTUFEE_01 rule implementation

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/HE/MSTUFEE/MSTUFEE_01Rule.cs
@@ -0,0 +1,52 @@
+﻿using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Data.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+using System.Collections.Generic;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.HE.MSTUFEE
+{
+    public class MSTUFEE_01Rule : AbstractRule, IRule<ILearner>
+    {
+        private readonly IProvideLookupDetails _provideLookupDetails;
+
+        public MSTUFEE_01Rule(IProvideLookupDetails provideLookupDetails, IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler, RuleNameConstants.MSTUFEE_01)
+        {
+            _provideLookupDetails = provideLookupDetails;
+        }
+
+        public void Validate(ILearner objectToValidate)
+        {
+            if (objectToValidate.LearningDeliveries == null)
+            {
+                return;
+            }
+
+            foreach (var learningDelivery in objectToValidate.LearningDeliveries)
+            {
+                if (learningDelivery.LearningDeliveryHEEntity != null && ConditionMet(learningDelivery.LearningDeliveryHEEntity.MSTUFEE))
+                {
+                    HandleValidationError(
+                        learnRefNumber: objectToValidate.LearnRefNumber,
+                        aimSequenceNumber: learningDelivery.AimSeqNumber,
+                        errorMessageParameters: BuildErrorMessageParameters(learningDelivery.LearningDeliveryHEEntity.MSTUFEE));
+                }
+            }
+        }
+
+        public bool ConditionMet(int mstufeeValue)
+        {
+            return !_provideLookupDetails.Contains(LookupSimpleKey.MSTuFee, mstufeeValue);
+        }
+
+        public IEnumerable<IErrorMessageParameter> BuildErrorMessageParameters(int mstufeeValue)
+        {
+            return new[]
+            {
+                BuildErrorMessageParameter(PropertyNameConstants.MSTUFEE, mstufeeValue)
+            };
+        }
+    }
+}
