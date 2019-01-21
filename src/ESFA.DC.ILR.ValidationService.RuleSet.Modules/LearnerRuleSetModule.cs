6170e651e97a4404b0c2bb60fa58801a02b3f064
Saqib <5689652+saqib2013@users.noreply.github.com>
2019-01-21 09:50:45 +0000

    AB#R116 - R116 validation rule condions implementation started.

--- src/ESFA.DC.ILR.ValidationService.RuleSet.Modules/LearnerRuleSetModule.cs
+++ src/ESFA.DC.ILR.ValidationService.RuleSet.Modules/LearnerRuleSetModule.cs
@@ -488,6 +488,7 @@ namespace ESFA.DC.ILR.ValidationService.RuleSet.Modules
                 typeof(R113Rule),
                 typeof(R114Rule),
                 typeof(R115Rule),
+                typeof(R116Rule),
                 typeof(R117Rule),
                 typeof(R118Rule),
                 typeof(Sex_01Rule),
---
+++ src/ESFA.DC.ILR.ValidationService.Rules/CrossEntity/R116Rule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.CrossEntity
+{
+    public class R116Rule : AbstractRule, IRule<ILearner>
+    {
+        private readonly int _fundModel = TypeOfFunding.ApprenticeshipsFrom1May2017;
+        private readonly HashSet<int> _progTypes = new HashSet<int>()
+        {
+            TypeOfLearningProgramme.AdvancedLevelApprenticeship,
+            TypeOfLearningProgramme.IntermediateLevelApprenticeship,
+            TypeOfLearningProgramme.HigherApprenticeshipLevel4,
+            TypeOfLearningProgramme.HigherApprenticeshipLevel5,
+            TypeOfLearningProgramme.HigherApprenticeshipLevel6,
+            TypeOfLearningProgramme.HigherApprenticeshipLevel7Plus
+        };
+
+        private readonly HashSet<int> _AFinTypes1And2 = new HashSet<int>()
+        {
+            ApprenticeshipFinancialRecord.PaymentRecordCodes.TrainingPayment,
+            ApprenticeshipFinancialRecord.PaymentRecordCodes.AssessmentPayment
+        };
+
+        public R116Rule(IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler, RuleNameConstants.R116)
+        {
+        }
+
+        public void Validate(ILearner objectToValidate)
+        {
+            return false;
+        }
+    }
+}
