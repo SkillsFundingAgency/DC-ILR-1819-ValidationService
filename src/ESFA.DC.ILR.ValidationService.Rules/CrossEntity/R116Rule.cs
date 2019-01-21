24ab8692d99c03b369e3168c647cb03538fda85e
Saqib <5689652+saqib2013@users.noreply.github.com>
2019-01-21 16:08:13 +0000

    AB#42708 - R116 validation rule conditions implemented.

--- src/ESFA.DC.ILR.ValidationService.Rules/CrossEntity/R116Rule.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/CrossEntity/R116Rule.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Data.Extensions;
 using ESFA.DC.ILR.ValidationService.Interface;
 using ESFA.DC.ILR.ValidationService.Rules.Abstract;
 using ESFA.DC.ILR.ValidationService.Rules.Constants;
@@ -13,7 +14,19 @@ namespace ESFA.DC.ILR.ValidationService.Rules.CrossEntity
     public class R116Rule : AbstractRule, IRule<ILearner>
     {
         private readonly int _fundModel = TypeOfFunding.ApprenticeshipsFrom1May2017;
-        private readonly HashSet<int> _progTypes = new HashSet<int>()
+        private readonly int _aimType = TypeOfAim.ProgrammeAim;
+        private readonly HashSet<int> _aFinCodes3 = new HashSet<int>()
+        {
+            ApprenticeshipFinancialRecord.PaymentRecordCodes.EmployerPaymentReimbursedByProvider
+        };
+
+        private readonly HashSet<int> _aFinCodes1And2 = new HashSet<int>()
+        {
+            ApprenticeshipFinancialRecord.PaymentRecordCodes.TrainingPayment,
+            ApprenticeshipFinancialRecord.PaymentRecordCodes.AssessmentPayment
+        };
+
+        private readonly HashSet<int?> _progTypes = new HashSet<int?>()
         {
             TypeOfLearningProgramme.AdvancedLevelApprenticeship,
             TypeOfLearningProgramme.IntermediateLevelApprenticeship,
@@ -23,12 +36,6 @@ namespace ESFA.DC.ILR.ValidationService.Rules.CrossEntity
             TypeOfLearningProgramme.HigherApprenticeshipLevel7Plus
         };
 
-        private readonly HashSet<int> _AFinTypes1And2 = new HashSet<int>()
-        {
-            ApprenticeshipFinancialRecord.PaymentRecordCodes.TrainingPayment,
-            ApprenticeshipFinancialRecord.PaymentRecordCodes.AssessmentPayment
-        };
-
         public R116Rule(IValidationErrorHandler validationErrorHandler)
             : base(validationErrorHandler, RuleNameConstants.R116)
         {
@@ -36,7 +43,49 @@ namespace ESFA.DC.ILR.ValidationService.Rules.CrossEntity
 
         public void Validate(ILearner objectToValidate)
         {
-            return false;
+            if (objectToValidate?.LearningDeliveries == null)
+            {
+                return;
+            }
+
+            foreach (var groupSums in objectToValidate.LearningDeliveries
+                .Where(
+                d => d.FundModel == _fundModel
+                && d.AimType == _aimType
+                && _progTypes.Contains(d.ProgTypeNullable)
+                && d.AppFinRecords != null)
+                .GroupBy(d => new
+                {
+                    progType = d.ProgTypeNullable,
+                    fWorkCode = d.FworkCodeNullable,
+                    pWayCode = d.PwayCodeNullable
+                }).ToList()
+                .Select(d => new
+                {
+                    aFinCode1And2Sum = GetSumsForAFinCode(d, _aFinCodes1And2),
+                    aFinCode3Sum = GetSumsForAFinCode(d, _aFinCodes3),
+                    aimSeqNumber = d.FirstOrDefault().AimSeqNumber
+                }))
+            {
+                if (IsAFinCodeSumNegative(groupSums.aFinCode1And2Sum, groupSums.aFinCode3Sum))
+                {
+                    HandleValidationError(
+                        learnRefNumber: objectToValidate.LearnRefNumber,
+                        aimSequenceNumber: groupSums.aimSeqNumber);
+                }
+            }
+        }
+
+        public bool IsAFinCodeSumNegative(int aFinCode1And2Sum, int aFinCode3Sum)
+        {
+            return (aFinCode1And2Sum - aFinCode3Sum) < 0;
+        }
+
+        public int GetSumsForAFinCode(IEnumerable<ILearningDelivery> learningDeliveries, HashSet<int> aFinCodes)
+        {
+            return learningDeliveries.SelectMany(a => a.AppFinRecords)
+                    .Where(f => f.AFinType.CaseInsensitiveEquals(ApprenticeshipFinancialRecord.Types.PaymentRecord)
+                        && aFinCodes.Contains(f.AFinCode)).Sum(f => f.AFinAmount);
         }
     }
 }
