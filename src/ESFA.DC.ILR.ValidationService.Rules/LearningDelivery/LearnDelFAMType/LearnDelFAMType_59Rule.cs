0c883f46d4e22cead5b2d3935a55c668cb1b0d24
Salman Baig <salman.sfa@outlook.com>
2019-01-22 11:47:54 +0000

    LearnDel famtype 59.. WIP

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/LearnDelFAMType/LearnDelFAMType_59Rule.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Data.Extensions;
+using ESFA.DC.ILR.ValidationService.Data.External.LARS.Interface;
+using ESFA.DC.ILR.ValidationService.Data.External.Organisation.Interface;
+using ESFA.DC.ILR.ValidationService.Data.File.FileData.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+using ESFA.DC.ILR.ValidationService.Rules.Derived.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Query.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.LearnDelFAMType
+{
+    public class LearnDelFAMType_59Rule : AbstractRule, IRule<ILearner>
+    {
+        private const double DaysInYear = 365.242199;
+
+        private const int MinAge = 19;
+        private const int MaxAge = 23;
+        private readonly DateTime MinimumStartDate = new DateTime(2016, 07, 31);
+        private readonly DateTime MaximumStartDate = new DateTime(2017, 08, 01);
+
+        private readonly HashSet<int> PriorAttainList = new HashSet<int>() { 2, 3, 4, 5, 10, 11, 12, 13, 97, 98 };
+        private readonly List<string> FamCodesForExclusion = new List<string>()
+        {
+            LearningDeliveryFAMCodeConstants.LDM_OLASS,
+            LearningDeliveryFAMCodeConstants.LDM_RoTL,
+            LearningDeliveryFAMCodeConstants.LDM_SteelRedundancy
+        };
+
+        private readonly HashSet<string> NvqLevelsList = new HashSet<string>(new List<string>() { "E", "1", "2" }).ToCaseInsensitiveHashSet();
+        private readonly HashSet<int> BasicSkillTypes = new HashSet<int>() { 01, 11, 13, 20, 23, 24, 29, 31, 02, 12, 14, 19, 21, 25, 30, 32, 33, 34, 35 };
+
+        private readonly ILARSDataService _larsDataService;
+        private readonly IDerivedData_07Rule _dd07;
+        private readonly IDerivedData_28Rule _dd28;
+        private readonly IDerivedData_29Rule _dd29;
+
+        private readonly ILearningDeliveryFAMQueryService _famQueryService;
+        private readonly IFileDataService _fileDataService;
+        private readonly IOrganisationDataService _organisationDataService;
+        private readonly IDateTimeQueryService _dateTimeQueryService;
+
+        public LearnDelFAMType_59Rule(
+            IValidationErrorHandler validationErrorHandler,
+            ILARSDataService larsDataService,
+            IDerivedData_07Rule dd07,
+            IDerivedData_28Rule dd28,
+            IDerivedData_29Rule dd29,
+            ILearningDeliveryFAMQueryService famQueryService,
+            IFileDataService fileDataService,
+            IOrganisationDataService organisationDataService,
+            IDateTimeQueryService dateTimeQueryService)
+            : base(validationErrorHandler, RuleNameConstants.LearnDelFAMType_56)
+        {
+            _larsDataService = larsDataService;
+            _dd07 = dd07;
+            _dd28 = dd28;
+            _dd29 = dd29;
+            _famQueryService = famQueryService;
+            _fileDataService = fileDataService;
+            _organisationDataService = organisationDataService;
+            _dateTimeQueryService = dateTimeQueryService;
+        }
+
+        public void Validate(ILearner learner)
+        {
+            if (learner?.LearningDeliveries == null)
+            {
+                return;
+            }
+
+            if (IsProviderExcluded())
+            {
+                return;
+            }
+
+            foreach (var learningDelivery in learner.LearningDeliveries)
+            {
+               if (ConditionMet(learningDelivery, learner.DateOfBirthNullable, learner.PriorAttainNullable) &&
+                   !IsLearningDeliveryExcluded(learner, learningDelivery))
+                {
+                    HandleValidationError(
+                        learningDelivery.LearnAimRef,
+                        learningDelivery.AimSeqNumber,
+                        BuildErrorMessageParameters(_fileDataService.UKPRN(), learner, learningDelivery));
+                }
+            }
+        }
+
+        public bool ConditionMet(ILearningDelivery learningDelivery, DateTime? dateofBirth, int? priorAttain)
+        {
+            return StartDateConditionMet(learningDelivery.LearnStartDate) &&
+                   FundModelConditionMet(learningDelivery.FundModel) &&
+                   AgeConditionMet(learningDelivery.LearnStartDate, dateofBirth) &&
+                   PriorAttainConditionMet(priorAttain) &&
+                   FamConditionMet(learningDelivery.LearningDeliveryFAMs) &&
+                   NvQLevelConditionMet(learningDelivery.LearnAimRef);
+        }
+
+        public bool StartDateConditionMet(DateTime learnStartDate)
+        {
+            return learnStartDate > MinimumStartDate && learnStartDate < MaximumStartDate;
+        }
+
+        public bool FundModelConditionMet(int fundModel)
+        {
+            return fundModel == TypeOfFunding.AdultSkills;
+        }
+
+        public bool AgeConditionMet(DateTime learnStartDate, DateTime? dateOfBirth)
+        {
+            if (!dateOfBirth.HasValue)
+            {
+                return false;
+            }
+
+            var ageAtCourseStart = _dateTimeQueryService.AgeAtGivenDate(dateOfBirth.Value, learnStartDate);
+            if (ageAtCourseStart >= MinAge && ageAtCourseStart <= MaxAge)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool FamConditionMet(IReadOnlyCollection<ILearningDeliveryFAM> fams)
+        {
+            return _famQueryService.HasLearningDeliveryFAMCodeForType(fams, LearningDeliveryFAMTypeConstants.FFI, LearningDeliveryFAMCodeConstants.FFI_Fully);
+        }
+
+        public bool PriorAttainConditionMet(int? priorAttain)
+        {
+            if (!priorAttain.HasValue)
+            {
+                return false;
+            }
+
+            return PriorAttainList.Contains(priorAttain.Value);
+        }
+
+        public bool NvQLevelConditionMet(string learnAimRef)
+        {
+            var nvqLevel = _larsDataService.GetNotionalNVQLevelv2ForLearnAimRef(learnAimRef);
+            return NvqLevelsList.Contains(nvqLevel);
+        }
+
+        public bool IsProviderExcluded()
+        {
+            return _organisationDataService.LegalOrgTypeMatchForUkprn(_fileDataService.UKPRN(), LegalOrgTypeConstants.USDC);
+        }
+
+        public bool IsLearningDeliveryExcluded(ILearner learner, ILearningDelivery learningDelivery)
+        {
+            if (learningDelivery.ProgTypeNullable.HasValue &&
+                learningDelivery.ProgTypeNullable.Value == TypeOfLearningProgramme.Traineeship)
+            {
+                return true;
+            }
+
+            if (_dd07.IsApprenticeship(learningDelivery.ProgTypeNullable))
+            {
+                return true;
+            }
+
+            if (_famQueryService.HasAnyLearningDeliveryFAMCodesForType(
+                learningDelivery.LearningDeliveryFAMs,
+                LearningDeliveryFAMTypeConstants.LDM,
+                FamCodesForExclusion))
+            {
+                return true;
+            }
+
+            if (_dd28.IsAdultFundedUnemployedWithBenefits(learner))
+            {
+                return true;
+            }
+
+            if (_dd29.IsInflexibleElementOfTrainingAim(learner))
+            {
+                return true;
+            }
+
+            if (_famQueryService.HasLearningDeliveryFAMType(learningDelivery.LearningDeliveryFAMs, LearningDeliveryFAMTypeConstants.RES))
+            {
+                return true;
+            }
+
+            if (_larsDataService.BasicSkillsMatchForLearnAimRefAndStartDate(
+                BasicSkillTypes,
+                learningDelivery.LearnAimRef,
+                learningDelivery.LearnStartDate))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public IEnumerable<IErrorMessageParameter> BuildErrorMessageParameters(long ukprn, ILearner learner, ILearningDelivery learningDelivery)
+        {
+            return new List<IErrorMessageParameter>
+            {
+                BuildErrorMessageParameter(PropertyNameConstants.UKPRN, ukprn),
+                BuildErrorMessageParameter(PropertyNameConstants.LearnAimRef, learningDelivery.LearnAimRef),
+                BuildErrorMessageParameter(PropertyNameConstants.ProgType, learningDelivery.ProgTypeNullable),
+                BuildErrorMessageParameter(PropertyNameConstants.FundModel, learningDelivery.FundModel),
+                BuildErrorMessageParameter(PropertyNameConstants.LearnDelFAMType, LearningDeliveryFAMTypeConstants.FFI),
+                BuildErrorMessageParameter(PropertyNameConstants.LearnDelFAMCode, LearningDeliveryFAMCodeConstants.FFI_Fully),
+                BuildErrorMessageParameter(PropertyNameConstants.LearnStartDate, learningDelivery.LearnStartDate),
+                BuildErrorMessageParameter(PropertyNameConstants.DateOfBirth, learner.DateOfBirthNullable),
+            };
+        }
+    }
+}
