436a80bc8698d83a8b858e45a8d25b8f072a67f7
Cory-Wynn <CoryWynn1993@gmail.com>
2019-01-22 13:58:57 +0000

    43169 SPECFEE_02

--- src/ESFA.DC.ILR.ValidationService.RuleSet.Modules/LearnerRuleSetModule.cs
+++ src/ESFA.DC.ILR.ValidationService.RuleSet.Modules/LearnerRuleSetModule.cs
@@ -507,6 +507,7 @@ namespace ESFA.DC.ILR.ValidationService.RuleSet.Modules
                 typeof(SEC_01Rule),
                 typeof(SEC_02Rule),
                 typeof(SPECFEE_01Rule),
+                typeof(SPECFEE_02Rule),
                 typeof(SOC2000_02Rule),
                 typeof(SWSupAimId_01Rule),
                 typeof(StdCode_01Rule),
--- src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/Constants/RuleNameConstants.cs
@@ -437,6 +437,7 @@
         public const string SEC_02 = "SEC_02";
 
         public const string SPECFEE_01 = "SPECFEE_01";
+        public const string SPECFEE_02 = "SPECFEE_02";
 
         public const string SOC2000_02 = "SOC2000_02";
 
