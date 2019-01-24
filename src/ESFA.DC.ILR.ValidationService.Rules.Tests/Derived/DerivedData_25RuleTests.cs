2c7ed41d058edd0c1a0b082b03cdabe540dfc453
jamie <jamie.coales@education.gov.uk>
2019-01-24 16:21:41 +0000

    DD25 feedback changes

--- src/ESFA.DC.ILR.ValidationService.Rules.Tests/Derived/DerivedData_25RuleTests.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules.Tests/Derived/DerivedData_25RuleTests.cs
@@ -14,20 +14,7 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.Derived
         [Fact]
         public void GetLearnersAgeAtStartOfESFContract_ReturnsNullForNullLearner()
         {
-            var serviceMock = new Mock<ILearnerEmploymentStatusQueryService>();
-            serviceMock
-                .Setup(m => m.LearnerEmploymentStatusForDate(It.IsAny<IEnumerable<ILearnerEmploymentStatus>>(), It.IsAny<DateTime>()))
-                .Returns(new TestLearnerEmploymentStatus
-                {
-                    EmploymentStatusMonitorings = new List<TestEmploymentStatusMonitoring>
-                    {
-                        new TestEmploymentStatusMonitoring
-                        {
-                            ESMType = "LOU",
-                            ESMCode = 5
-                        }
-                    }
-                });
+            var serviceMock = GetServiceMock();
 
             var rule = NewRule(serviceMock.Object);
 
@@ -37,22 +24,54 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.Derived
         }
 
         [Fact]
-        public void GetLearnersAgeAtStartOfESFContract_ReturnsNullForNullLearningDeliveries()
+        public void GetLearnersAgeAtStartOfESFContract_ServiceCalledWithCorrectParams()
         {
-            var serviceMock = new Mock<ILearnerEmploymentStatusQueryService>();
-            serviceMock
-                .Setup(m => m.LearnerEmploymentStatusForDate(It.IsAny<IEnumerable<ILearnerEmploymentStatus>>(), It.IsAny<DateTime>()))
-                .Returns(new TestLearnerEmploymentStatus
+            var employmentStatuses = new List<TestLearnerEmploymentStatus>
+            {
+                new TestLearnerEmploymentStatus
                 {
                     EmploymentStatusMonitorings = new List<TestEmploymentStatusMonitoring>
                     {
                         new TestEmploymentStatusMonitoring
                         {
                             ESMType = "LOU",
-                            ESMCode = 5
+                            ESMCode = 0
                         }
                     }
-                });
+                }
+            };
+
+            var serviceMock = GetServiceMock();
+
+            var rule = NewRule(serviceMock.Object);
+
+            var testConRefNumber = "123456";
+            var startDate = new DateTime(2017, 9, 1);
+
+            var learner = new TestLearner
+            {
+                LearnerEmploymentStatuses = employmentStatuses,
+                LearningDeliveries = new List<TestLearningDelivery>
+                {
+                    new TestLearningDelivery
+                    {
+                        ConRefNumber = testConRefNumber,
+                        LearnStartDate = startDate,
+                        LearnAimRef = "ZESF0001",
+                        CompStatus = 2
+                    }
+                }
+            };
+
+            rule.GetLengthOfUnemployment(learner, testConRefNumber);
+
+            serviceMock.Verify(m => m.LearnerEmploymentStatusForDate(employmentStatuses, startDate), Times.Once);
+        }
+
+        [Fact]
+        public void GetLearnersAgeAtStartOfESFContract_ReturnsNullForNullLearningDeliveries()
+        {
+            var serviceMock = GetServiceMock();
 
             var rule = NewRule(serviceMock.Object);
             var learner = new TestLearner
@@ -68,27 +87,13 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.Derived
         [Fact]
         public void GetLearnersAgeAtStartOfESFContract_ReturnsNullForIrrelevantConRefNumber()
         {
-            var serviceMock = new Mock<ILearnerEmploymentStatusQueryService>();
-            serviceMock
-                .Setup(m => m.LearnerEmploymentStatusForDate(It.IsAny<IEnumerable<ILearnerEmploymentStatus>>(), It.IsAny<DateTime>()))
-                .Returns(new TestLearnerEmploymentStatus
-                {
-                    EmploymentStatusMonitorings = new List<TestEmploymentStatusMonitoring>
-                    {
-                        new TestEmploymentStatusMonitoring
-                        {
-                            ESMType = "LOU",
-                            ESMCode = 5
-                        }
-                    }
-                });
+            var serviceMock = GetServiceMock();
 
             var rule = NewRule(serviceMock.Object);
 
             var testConRefNumber = "123456";
             var learner = new TestLearner
             {
-                DateOfBirthNullable = new DateTime(1999, 9, 1),
                 LearningDeliveries = new List<TestLearningDelivery>
                 {
                     new TestLearningDelivery
@@ -109,27 +114,13 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.Derived
         [Fact]
         public void GetLearnersAgeAtStartOfESFContract_ReturnsNullForNoZESF0001Deliverable()
         {
-            var serviceMock = new Mock<ILearnerEmploymentStatusQueryService>();
-            serviceMock
-                .Setup(m => m.LearnerEmploymentStatusForDate(It.IsAny<IEnumerable<ILearnerEmploymentStatus>>(), It.IsAny<DateTime>()))
-                .Returns(new TestLearnerEmploymentStatus
-                {
-                    EmploymentStatusMonitorings = new List<TestEmploymentStatusMonitoring>
-                    {
-                        new TestEmploymentStatusMonitoring
-                        {
-                            ESMType = "LOU",
-                            ESMCode = 5
-                        }
-                    }
-                });
+            var serviceMock = GetServiceMock();
 
             var rule = NewRule(serviceMock.Object);
 
             var testConRefNumber = "123456";
             var learner = new TestLearner
             {
-                DateOfBirthNullable = new DateTime(1999, 9, 1),
                 LearningDeliveries = new List<TestLearningDelivery>
                 {
                     new TestLearningDelivery
@@ -150,27 +141,13 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.Derived
         [Fact]
         public void GetLearnersAgeAtStartOfESFContract_ReturnsNullForNoZESF0001DeliverableCompleted()
         {
-            var serviceMock = new Mock<ILearnerEmploymentStatusQueryService>();
-            serviceMock
-                .Setup(m => m.LearnerEmploymentStatusForDate(It.IsAny<IEnumerable<ILearnerEmploymentStatus>>(), It.IsAny<DateTime>()))
-                .Returns(new TestLearnerEmploymentStatus
-                {
-                    EmploymentStatusMonitorings = new List<TestEmploymentStatusMonitoring>
-                    {
-                        new TestEmploymentStatusMonitoring
-                        {
-                            ESMType = "LOU",
-                            ESMCode = 5
-                        }
-                    }
-                });
+            var serviceMock = GetServiceMock();
 
             var rule = NewRule(serviceMock.Object);
 
             var testConRefNumber = "123456";
             var learner = new TestLearner
             {
-                DateOfBirthNullable = new DateTime(1999, 9, 1),
                 LearningDeliveries = new List<TestLearningDelivery>
                 {
                     new TestLearningDelivery
@@ -192,27 +169,13 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.Derived
         public void GetLearnersAgeAtStartOfESFContract_ReturnsCorrectAge()
         {
             var expectedResult = 5;
-            var serviceMock = new Mock<ILearnerEmploymentStatusQueryService>();
-            serviceMock
-                .Setup(m => m.LearnerEmploymentStatusForDate(It.IsAny<IEnumerable<ILearnerEmploymentStatus>>(), It.IsAny<DateTime>()))
-                .Returns(new TestLearnerEmploymentStatus
-                {
-                    EmploymentStatusMonitorings = new List<TestEmploymentStatusMonitoring>
-                    {
-                        new TestEmploymentStatusMonitoring
-                        {
-                            ESMType = "LOU",
-                            ESMCode = expectedResult
-                        }
-                    }
-                });
+            var serviceMock = GetServiceMock(expectedResult);
 
             var rule = NewRule(serviceMock.Object);
 
             var testConRefNumber = "123456";
             var learner = new TestLearner
             {
-                DateOfBirthNullable = new DateTime(1999, 9, 1),
                 LearningDeliveries = new List<TestLearningDelivery>
                 {
                     new TestLearningDelivery
@@ -233,6 +196,26 @@ namespace ESFA.DC.ILR.ValidationService.Rules.Tests.Derived
         public DerivedData_25Rule NewRule(ILearnerEmploymentStatusQueryService learnerEmploymentStatusQueryService = null)
         {
             return new DerivedData_25Rule(learnerEmploymentStatusQueryService);
+        }
+
+        private Mock<ILearnerEmploymentStatusQueryService> GetServiceMock(int expectedResult = 0)
+        {
+            var serviceMock = new Mock<ILearnerEmploymentStatusQueryService>();
+            serviceMock
+                .Setup(m => m.LearnerEmploymentStatusForDate(It.IsAny<IEnumerable<ILearnerEmploymentStatus>>(), It.IsAny<DateTime>()))
+                .Returns(new TestLearnerEmploymentStatus
+                {
+                    EmploymentStatusMonitorings = new List<TestEmploymentStatusMonitoring>
+                    {
+                        new TestEmploymentStatusMonitoring
+                        {
+                            ESMType = "LOU",
+                            ESMCode = expectedResult
+                        }
+                    }
+                });
+
+            return serviceMock;
         }
     }
 }
