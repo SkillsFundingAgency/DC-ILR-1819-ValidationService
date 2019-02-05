3b83bbf26cc34697df7939489d8c4cbbd46be454
jamie <jamie.coales@education.gov.uk>
2019-02-05 10:55:00 +0000

    ESMType14 tidy using statements

--- src/ESFA.DC.ILR.ValidationService.Rules.Tests/EmploymentStatus/ESMType/ESMType_14RuleTests.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules.Tests/EmploymentStatus/ESMType/ESMType_14RuleTests.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using ESFA.DC.ILR.Model.Interface;
-using ESFA.DC.ILR.Tests.Model;
 using ESFA.DC.ILR.ValidationService.Data.External.FCS.Interface;
 using ESFA.DC.ILR.ValidationService.Interface;
 using ESFA.DC.ILR.ValidationService.Rules.Constants;
-using ESFA.DC.ILR.ValidationService.Rules.Derived;
 using ESFA.DC.ILR.ValidationService.Rules.Derived.Interface;
 using ESFA.DC.ILR.ValidationService.Rules.EmploymentStatus.ESMType;
-using ESFA.DC.ILR.ValidationService.Rules.Query;
 using ESFA.DC.ILR.ValidationService.Rules.Query.Interface;
 using ESFA.DC.ILR.ValidationService.Utility;
 using Moq;
 using Xunit;
-using It = Moq.It;
 
 namespace ESFA.DC.ILR.ValidationService.Rules.Tests.EmploymentStatus.ESMType
 {
